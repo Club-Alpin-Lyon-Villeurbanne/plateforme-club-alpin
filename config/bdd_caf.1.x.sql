f482ba209f957c5af961f5fa9dbfbb6bd24d3580
Sylvain Maillard <sylvain@ecologeek.net>
2016-01-02 23:58:43 +0100

    Précision : il faut être inscrit au CAF local pour pouvoir activer son compte.

--- config/bdd_caf.1.x.sql
+++ config/bdd_caf.1.x.sql
@@ -3,3 +3,11 @@
 -- 
 ALTER TABLE `caf_user` CHANGE `birthday_user` `birthday_user` BIGINT(20) NULL DEFAULT NULL; 
 
+
+--
+-- Modification dans la table `caf_content_html`
+-- Précision : besoin d'être inscrit au CAF Lyon-Villeurbanne
+--
+
+UPDATE `caf_content_html` SET `contenu_content_html` = '<p class="menutitle">Activer mon compte</p> <p>Pour rejoindre le site, vous devez être inscrit au Club Alpin Français de Lyon-Villeurbanne ou sa section Ouest Lyonnais.<br />Munissez-vous de votre numéro d''adhérent et de votre adresse e-mail, choisissez un peudonyme et un mot de passe, et laissez-vous guider.</p>' WHERE `id_content_html` = 2
+
