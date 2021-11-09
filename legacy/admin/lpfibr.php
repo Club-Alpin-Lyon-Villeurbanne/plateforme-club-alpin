a065ad55b100b63b0968e3c41dfe6db09bdb563c
Romain Neutron <romain@neutron.io>
2021-11-09 08:32:25 +0100

    Fix paths

--- legacy/admin/lpfibr.php
+++ legacy/admin/lpfibr.php
@@ -15,9 +15,9 @@ if (admin()) {
     $type = $_GET['type'];
     $dossier = null;
     if ('image' == $type) {
-        $dossier = __DIR__.'/../public/ftp/images/';
+        $dossier = __DIR__.'/../../public/ftp/images/';
     } elseif ('file' == $type) {
-        $dossier = __DIR__.'/../public/ftp/telechargements/';
+        $dossier = __DIR__.'/../../public/ftp/telechargements/';
     } else {
         echo "ERREUR : type invalide ($type / $dossier)";
         exit();
