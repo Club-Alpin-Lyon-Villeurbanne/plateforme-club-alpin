460be684c469f6e4c03cb597bda71ba5c782f6bd
Romain Neutron <romain@neutron.io>
2021-11-21 13:55:46 +0100

    Fix migrations

--- migrations/Version20211121115308.php
+++ migrations/Version20211121115308.php
@@ -12,8 +12,10 @@ final class Version20211121115308 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE caf_user CHANGE mdp_user mdp_user VARCHAR(1024) NOT NULL');
+        $this->addSql('ALTER TABLE caf_user CHANGE email_user email_user VARCHAR(200) DEFAULT NULL');
+        $this->addSql('UPDATE caf_user SET email_user = NULL WHERE email_user = \'\'');
+        $this->addSql('ALTER TABLE caf_user CHANGE mdp_user mdp_user VARCHAR(1024) NOT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_DEBE826812A5F6CC ON caf_user (email_user)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_DEBE82686A22D67B ON caf_user (cafnum_user)');
     }
 
     public function down(Schema $schema): void
@@ -21,5 +23,6 @@ final class Version20211121115308 extends AbstractMigration
         $this->addSql('DROP INDEX UNIQ_DEBE826812A5F6CC ON caf_user');
         $this->addSql('DROP INDEX UNIQ_DEBE82686A22D67B ON caf_user');
         $this->addSql('ALTER TABLE caf_user CHANGE mdp_user mdp_user VARCHAR(32) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
+        $this->addSql('ALTER TABLE caf_user CHANGE email_user email_user VARCHAR(200) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
     }
 }
