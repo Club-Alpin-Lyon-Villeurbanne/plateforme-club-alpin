306cbbf5fb3d836be86651c9b21cce44410c657a
Romain Neutron <romain@neutron.io>
2021-11-21 15:50:53 +0100

    Add sentry support in symfony context

---
+++ src/Bridge/Monolog/ActivationStrategy/HttpKernelExceptionActivationStrategy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Bridge\Monolog\ActivationStrategy;
+
+use Monolog\Handler\FingersCrossed\ActivationStrategyInterface;
+use Monolog\Logger;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class HttpKernelExceptionActivationStrategy implements ActivationStrategyInterface
+{
+    public const EXCLUDED_STATUS_CODES = [
+        400,
+        401,
+        402, // Payment required
+        403,
+        404,
+        405,
+        410, // Gone
+        412, // Precondition failed
+        423, // Locked
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isHandlerActivated(array $record): bool
+    {
+        $default = $record['level'] >= Logger::ERROR;
+
+        if (!isset($record['context']['exception'])) {
+            return $default;
+        }
+
+        $exception = $record['context']['exception'];
+
+        if ($exception instanceof \Exception) {
+            do {
+                if ($exception instanceof HttpExceptionInterface) {
+                    return $this->isStatusCodeActive($exception->getStatusCode());
+                }
+
+                if ($exception instanceof AccessDeniedException) {
+                    return $this->isStatusCodeActive($exception->getCode());
+                }
+            } while ($exception = $exception->getPrevious());
+        }
+
+        return $default;
+    }
+
+    private function isStatusCodeActive(int $statusCode): bool
+    {
+        return !\in_array($statusCode, self::EXCLUDED_STATUS_CODES, true);
+    }
+}
---
+++ src/Bridge/Monolog/Processor/UserProcessor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Bridge\Monolog\Processor;
+
+use Monolog\Processor\ProcessorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+/**
+ * This class adds user information to the Monolog records when user is connected.
+ */
+class UserProcessor implements ProcessorInterface
+{
+    private TokenStorageInterface $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * Adds user information to a record.
+     */
+    public function __invoke(array $record): array
+    {
+        if (!$this->tokenStorage->getToken()) {
+            return $record;
+        }
+
+        $user = $this->tokenStorage->getToken()->getUser();
+        if (!$user) {
+            return $record;
+        }
+
+        if (method_exists($user, 'getUuid')) {
+            $record['uuid'] = $user->getUuid();
+        }
+        if (method_exists($user, 'getEmail')) {
+            $record['email'] = $user->getEmail();
+        }
+
+        if (method_exists($user, 'getProviderUsername')) {
+            $record['username'] = $user->getProviderUsername();
+        } elseif (method_exists($user, 'getUserIdentifier')) {
+            $record['username'] = $user->getUserIdentifier();
+        } elseif (method_exists($user, 'getUsername')) {
+            $record['username'] = $user->getUsername();
+        }
+
+        return $record;
+    }
+}
