f818931f30568be2001a389f3a831c627cba40a4
Romain Neutron <romain@neutron.io>
2021-12-09 22:45:46 +0100

    Fix password renewal when connected with the remember me token

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Validator\CompliantPassword;
+use App\Validator\Recaptcha;
+use App\Validator\UserPasswordValidator;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('current_password', PasswordType::class, [
+                'constraints' => new UserPassword(null, null, UserPasswordValidator::class),
+                'label' => ' ',
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'type1', 'placeholder' => 'Mot de passe actuel'],
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => [
+                    'label' => ' ',
+                    'constraints' => new CompliantPassword(),
+                    'attr' => ['autocomplete' => 'new-password', 'class' => 'type1', 'placeholder' => 'Nouveau mot de passe'],
+                ],
+                'second_options' => [
+                    'label' => ' ',
+                    'attr' => ['autocomplete' => 'new-password', 'class' => 'type1', 'placeholder' => 'Confirmation du nouveau mot de passe'],
+                ],
+            ])
+            ->add('recaptcha', HiddenType::class, [
+                'constraints' => new Recaptcha('changePassword'),
+            ])
+        ;
+    }
+}
