4c6db0c1a4e83b4afd797aaa6a0ec990e14d35c3
Romain Neutron <romain@neutron.io>
2021-12-21 21:35:05 +0100

    Twigify the right column

---
+++ src/Bridge/Twig/TwigExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Bridge\Twig;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TwigExtension extends AbstractExtension implements ServiceSubscriberInterface
+{
+    private ContainerInterface $locator;
+
+    public function __construct(ContainerInterface $locator)
+    {
+        $this->locator = $locator;
+    }
+
+    public static function getSubscribedServices()
+    {
+        return [
+            SluggerInterface::class,
+        ];
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('slugify', [$this, 'slugify']),
+            new TwigFilter('random_item', [$this, 'getRandomItem']),
+        ];
+    }
+
+    public function slugify(string $string)
+    {
+        return $this->locator->get(SluggerInterface::class)->slug($string);
+    }
+
+    public function getRandomItem(array $items)
+    {
+        if (empty($items)) {
+            return null;
+        }
+
+        return $items[array_rand($items)];
+    }
+}
