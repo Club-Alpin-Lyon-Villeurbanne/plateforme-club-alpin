105434c75a47af1820cd33843076ff07bfdbbaf7
Romain Neutron <romain@neutron.io>
2022-01-05 21:24:17 +0100

    Few fixes

--- legacy/includes/evt-fiche.php
+++ legacy/includes/evt-fiche.php
@@ -322,10 +322,8 @@ if (!$evt) {
                 // MAP TRANSPORT?>
                 <div id="map-fichesortie"></div>
                 <!-- ****************** // CARTE RDV -->
-                    <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
-                        integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
-                        crossorigin="">
-                    </script>
+                    <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"></script>
+                    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" type="text/css"  media="screen" />
                     <script>
                     var lat            = <?php echo str_replace(',', '.', (float) ($evt['lat_evt'])); ?>;
                     var lon            = <?php echo str_replace(',', '.', (float) ($evt['long_evt'])); ?>;
