aa341f0e46653a1bdba92c2dfd96543ea71a8dc4
Nicolas Ritouet <nicolas@ritouet.com>
2022-07-23 20:36:43 +0200

    Infra: modify continuous deployment for aws migration

---
+++ legacy/config/www.clubalpinlyon.top/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'sentry_dsn' => 'https://eaa373582defe4133aa456361e19f0518@o1046113.ingest.sentry.io/6021900',
+    'https' => true,
+    'url' => 'https://www.clubalpinlyon.top',
+    'use_smtp' => true,
+    'smtp_conf' => [
+        'host' => 'smtp.eu.mailgun.org',
+        'port' => 465,
+        'ssl' => true,
+        'user' => 'postmaster@mg.clubalpinlyon.fr',
+        'pass' => 'dc23887c4a62479ee5b0a7d32932ac0a-7005f37e-b40a7863',
+    ],
+    'db' => [
+        'host' => 'caflv-dev-aurora-mysql.cluster-cw75ek4t1pty.eu-west-3.rds.amazonaws.com',
+        'port' => 3306,
+        'user' => 'DB_USER_TO_REPLACE',
+        'password' => 'DB_PASSWORD_TO_REPLACE',
+        'dbname' => 'DB_NAME_TO_REPLACE',
+    ],
+];
