ccdd12e9ccc7d5e0a63664e35fc6e875b9fa98d3
Romain Neutron <romain@neutron.io>
2023-04-18 23:44:06 +0200

    Allow user to register to an event at any time

--- legacy/includes/evt/user_inscription.php
+++ legacy/includes/evt/user_inscription.php
@@ -8,54 +8,49 @@ if (user() && allowed('evt_join')) {
     if (!getUser()->getDoitRenouveler()) {
         // sortie pas passée
         if ($evt['tsp_evt'] > time()) {
-            // sortie dans plus de deux jours
-            if ($evt['tsp_evt'] > strtotime('midnight +2 days')) {
-                // inscriptions démarrées
-                if ($evt['join_start_evt'] < time()) {
-                    // Je ne suis pas déja inscrit (ou bien je dispose de filiations à inscrire)
-                    if ('neutre' == $monStatut || count($filiations)) {
-                        require __DIR__.'/../../includes/evt/user_inscription_button.php'; ?>
+            // inscriptions démarrées
+            if ($evt['join_start_evt'] < time()) {
+                // Je ne suis pas déja inscrit (ou bien je dispose de filiations à inscrire)
+                if ('neutre' == $monStatut || count($filiations)) {
+                    require __DIR__.'/../../includes/evt/user_inscription_button.php'; ?>
 
-                        <div id="inscription" style="display:<?php if (isset($_POST['operation']) && 'user_join' == $_POST['operation']) {
-                            echo 'block';
-                        } else {
-                            echo 'none';
-                        } ?>">
+                    <div id="inscription" style="display:<?php if (isset($_POST['operation']) && 'user_join' == $_POST['operation']) {
+                        echo 'block';
+                    } else {
+                        echo 'none';
+                    } ?>">
 
-                            <?php /* messages informatifs */ inclure('formalites-inscription', 'formalites'); ?>
+                        <?php /* messages informatifs */ inclure('formalites-inscription', 'formalites'); ?>
 
-                            <?php
-                                // TABLEAU d'erreurs
-                                if (isset($_POST['operation']) && 'user_join' == $_POST['operation'] && isset($errTab) && count($errTab) > 0) {
-                                    echo '<div class="erreur">Erreur : <ul><li>'.implode('</li><li>', $errTab).'</li></ul></div>';
-                                }
-                        if (isset($_POST['operation']) && 'user_join' == $_POST['operation'] && (!isset($errTab) || 0 === count($errTab))) {
-                            echo '<div class="info">Opération effectuée avec succès : '.count($inscrits).' personnes pré-inscrite(s)</div>';
-                        } ?>
-
-                            <form action="<?php echo $versCettePage; ?>#inscription" method="post" class="loading">
-
-                                <input type="hidden" name="operation" value="user_join" />
-                                <input type="hidden" name="id_evt" value="<?php echo $id_evt; ?>" />
-
-                                <div class="check-nice ">
-
-                                    <?php require __DIR__.'/../../includes/evt/user_inscription_options.php'; ?>
-
-                                </div>
-
-                                <?php require __DIR__.'/../../includes/evt/user_inscription_submit.php'; ?>
-
-                            </form>
-
-                        </div>
                         <?php
-                    }
-                } else {
-                    echo '<hr /><h2>Inscriptions :</h2><p>Les inscriptions pour cette sortie commenceront le '.date('d/m/y', $evt['join_start_evt']).'.</p>';
+                            // TABLEAU d'erreurs
+                            if (isset($_POST['operation']) && 'user_join' == $_POST['operation'] && isset($errTab) && count($errTab) > 0) {
+                                echo '<div class="erreur">Erreur : <ul><li>'.implode('</li><li>', $errTab).'</li></ul></div>';
+                            }
+                    if (isset($_POST['operation']) && 'user_join' == $_POST['operation'] && (!isset($errTab) || 0 === count($errTab))) {
+                        echo '<div class="info">Opération effectuée avec succès : '.count($inscrits).' personnes pré-inscrite(s)</div>';
+                    } ?>
+
+                        <form action="<?php echo $versCettePage; ?>#inscription" method="post" class="loading">
+
+                            <input type="hidden" name="operation" value="user_join" />
+                            <input type="hidden" name="id_evt" value="<?php echo $id_evt; ?>" />
+
+                            <div class="check-nice ">
+
+                                <?php require __DIR__.'/../../includes/evt/user_inscription_options.php'; ?>
+
+                            </div>
+
+                            <?php require __DIR__.'/../../includes/evt/user_inscription_submit.php'; ?>
+
+                        </form>
+
+                    </div>
+                    <?php
                 }
             } else {
-                inclure('info-inscription-moins-deux-jours', 'vide');
+                echo '<hr /><h2>Inscriptions :</h2><p>Les inscriptions pour cette sortie commenceront le '.date('d/m/y', $evt['join_start_evt']).'.</p>';
             }
         } else {
             inclure('info-inscription-passee', 'vide');
