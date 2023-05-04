8d1a101407ce4f007e11c5b442bba5fda5ef1c5e
Romain Neutron <romain@neutron.io>
2023-05-04 12:20:49 +0200

    Fix migrafion

--- migrations/Version20230504100402.php
+++ migrations/Version20230504100402.php
@@ -19,9 +19,9 @@ final class Version20230504100402 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE messenger_messages MODIFY body longtext COLLATE utf8mb4_unicode_ci NOT NULL');
-        $this->addSql('ALTER TABLE messenger_messages MODIFY headers longtext COLLATE utf8mb4_unicode_ci NOT NULL');
-        $this->addSql('ALTER TABLE messenger_messages MODIFY queue_name varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL');
+        $this->addSql('IF  EXISTS(SELECT NULL FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'messenger_messages\' AND table_schema = \''.$this->connection->getDatabase().'\') THEN ALTER TABLE messenger_messages MODIFY body longtext COLLATE utf8mb4_unicode_ci NOT NULL; END IF');
+        $this->addSql('IF  EXISTS(SELECT NULL FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'messenger_messages\' AND table_schema = \''.$this->connection->getDatabase().'\') THEN ALTER TABLE messenger_messages MODIFY headers longtext COLLATE utf8mb4_unicode_ci NOT NULL; END IF');
+        $this->addSql('IF  EXISTS(SELECT NULL FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'messenger_messages\' AND table_schema = \''.$this->connection->getDatabase().'\') THEN ALTER TABLE messenger_messages MODIFY queue_name varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL; END IF');
     }
 
     public function down(Schema $schema): void
