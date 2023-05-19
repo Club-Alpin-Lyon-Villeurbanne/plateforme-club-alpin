a9766119d7967677f2ec61be20fa3de73a0ae57d
Nicolas Ritouet <nicolas@ritouet.com>
2023-05-19 08:12:43 +0200

    Feat: supprime le bouton permettant de supprimer une sortie passée

--- legacy/includes/evt-tools.php
+++ legacy/includes/evt-tools.php
@@ -16,8 +16,8 @@ if ($evt) {
         .(allowed('evt_cancel', 'commission:'.$evt['code_commission']) && 1 == $evt['status_evt'] && $evt['tsp_evt'] > time() && 0 == $evt['cancelled_evt'] ? '<a class="nice2 red" href="/annuler-une-sortie/'.html_utf8($evt['code_evt']).'-'.(int) $evt['id_evt'].'.html" title="Annuler la sortie ci-dessous">Annuler</a>' : '')
 
         // SUPPRIMER
-        // on ne peut supprimer que si elle n'est pas publiée OU annulée OU passée
-        .(((allowed('evt_delete') || $evt['user_evt'] == getUser()->getId()) && ($evt['cancelled_evt'] || 1 != $evt['status_evt'] || $evt['tsp_end_evt'] < time() || null === $evt['tsp_end_evt']))
+        // on ne peut supprimer que si elle n'est pas publiée OU annulée
+        .(((allowed('evt_delete') || $evt['user_evt'] == getUser()->getId()) && ($evt['cancelled_evt'] || 1 != $evt['status_evt']))
             ? '<a class="nice2 red" href="/supprimer-une-sortie/'.html_utf8($evt['code_evt']).'-'.(int) $evt['id_evt'].'.html" title="Supprimer définitivement la sortie ci-dessous">Supprimer</a>' : '')
 
     .'</div>';
