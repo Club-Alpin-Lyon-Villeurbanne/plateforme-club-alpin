f8d36b32d2998977169d386f986b3758d93fbde0
GaetanTrema <gaetantrema@gmail.com>
2023-11-27 09:40:23 +0100

    add utils

---
+++ src/Utils/Serialize/ExpenseFieldTypeSerializer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Utils\Serialize;
+
+use App\Entity\ExpenseFieldType;
+
+class ExpenseFieldTypeSerializer {
+
+    public static function serialize(ExpenseFieldType $expenseFieldType): ?array
+    {
+        return [
+            'id' => $expenseFieldType->getId(),
+            'name' => $expenseFieldType->getName(),
+            'slug' => $expenseFieldType->getSlug(),
+            // property set manually in SortieController.php
+            'needsJustification' => $expenseFieldType->getNeedsJustification(),
+        ];
+    }
+}
