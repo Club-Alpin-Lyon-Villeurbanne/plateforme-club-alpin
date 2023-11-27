de2a81fb31d76aec96b45c1471b5ddbac40782e4
GaetanTrema <gaetantrema@gmail.com>
2023-11-27 16:45:03 +0100

    fix migration class name

--- migrations/Version20231127163000.php
+++ migrations/Version20231127163000.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231030093924 extends AbstractMigration
+final class Version20231127163000 extends AbstractMigration
 {
     public function getDescription(): string
     {
