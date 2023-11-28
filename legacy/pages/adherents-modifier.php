54d717e0e7fc23e220d9ec6bca7eb9eaccb8f028
manekineko17 <mgrouiller@gmail.com>
2023-11-28 11:31:18 +0100

    modif

--- legacy/pages/adherents-modifier.php
+++ legacy/pages/adherents-modifier.php
@@ -3,20 +3,26 @@
 use App\Legacy\LegacyContainer;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-$idUser = isset($userTab['id_user']) ?? null;
-$civUser = isset($userTab['civ_user']) ?? null;
-$firstnameUser = isset($userTab['firstname_user']) ??  null;
-$lastnameUser = isset($userTab['lastname_user']) ?? null;
-$nicknameUser = isset($userTab['nickname_user']) ?? null;
-$alerteRenouvelerUser = isset($userTab['alerte_renouveler_user']) ?? null;
-$dateAdhesionUser = isset($userTab['date_adhesion_user']) ?? null;
-$birthdayUser = isset($userTab['birthday_user']) ?? null;
-$telUser = isset($userTab['tel_user']) ?? null;
-$tel2User = isset($userTab['tel2_user']) ?? null;
-$adresseUser = isset($userTab['adresse_user']) ?? null;
-$cpUser = isset($userTab['cp_user']) ?? null;
-$villeUser = isset($userTab['ville_user']) ?? null;
-$paysUser = isset($userTab['pays_user']) ?? null;
+$idUser = $userTab['id_user'] ?? null;
+$civUser = $userTab['civ_user'] ?? null;
+$firstnameUser = $userTab['firstname_user'] ??  null;
+$lastnameUser = $userTab['lastname_user'] ?? null;
+$nicknameUser = $userTab['nickname_user'] ?? null;
+$alerteRenouvelerUser = $userTab['alerte_renouveler_user'] ?? null;
+$dateAdhesionUser = $userTab['date_adhesion_user'] ?? null;
+$birthdayUser = $userTab['birthday_user'] ?? null;
+$telUser = $userTab['tel_user'] ?? null;
+$tel2User = $userTab['tel2_user'] ?? null;
+$adresseUser = $userTab['adresse_user'] ?? null;
+$cpUser = $userTab['cp_user'] ?? null;
+$villeUser = $userTab['ville_user'] ?? null;
+$paysUser = $userTab['pays_user'] ?? null;
+$validUser = $userTab['valid_user'] ?? null;
+$cafnumUserNew = $userTab['cafnum_user_new'] ?? null;
+$cafnumeUser = $userTab['cafnum_user'] ?? null;
+$authContactUser = $userTab['auth_contact_user'] ?? null;
+$operation = $_POST['operation'] ?? null;
+$userEmail = $userTab['email_user'] ?? null;
 
 if (!admin() && !allowed('user_edit_notme')) {
     echo 'Vos droits ne sont pas assez élevés pour accéder à cette page';
@@ -55,10 +61,10 @@ if (!admin() && !allowed('user_edit_notme')) {
 		<?php
 
         // TABLEAU
-        if (isset($_POST['operation']) && 'user_edit' == $_POST['operation'] && isset($errTab) && count($errTab) > 0) {
+        if ('user_edit' == $operation && isset($errTab) && count($errTab) > 0) {
             echo '<div class="erreur">Erreur : <ul><li>'.implode('</li><li>', $errTab).'</li></ul></div>';
         }
-    if (isset($_POST['operation']) && 'user_edit' == $_POST['operation'] && (!isset($errTab) || 0 === count($errTab))) {
+    if ('user_edit' == $operation && (!isset($errTab) || 0 === count($errTab))) {
         echo '<div class="info"><b>Adhérent modifié avec succès :</b> <ul><li>'.implode('</li><li>', $okTab).'</li></ul></div>';
     } else {
         ?>
@@ -95,14 +101,14 @@ if (!admin() && !allowed('user_edit_notme')) {
 			</b><br />
 
 			<?php
-                if (isset($userTab['valid_user']) && 1 != $userTab['valid_user']) {
-                    // compte non actif pour le moment
+                if ($validUser && 1 != $validUser) {
+                    // compte non actif pour le moment (0 inactif, 1 actif)
                     echo '<br />URL d\'activation du compte : '.LegacyContainer::get('legacy_router')->generate('legacy_root', [], UrlGeneratorInterface::ABSOLUTE_URL).'user-confirm/'.$userTab['cookietoken_user'].'-'.$idUser.'.html<br />';
                 } ?>
 
 			<br />
 			E-mail* :<br />
-			<input type="text" name="email_user" class="type1" value="<?php echo $userTab['email_user']; ?>" placeholder="" />
+			<input type="text" name="email_user" class="type1" value="<?php echo $userEmail; ?>" placeholder="" />
 			<br />
 
 			Mot de passe :<br />
@@ -114,8 +120,8 @@ if (!admin() && !allowed('user_edit_notme')) {
 			<br />
  -->
 			Numéro de licence :<br />
-			<input type="text" name="cafnum_user" class="type1" value="<?php echo $userTab['cafnum_user']; ?>" placeholder="" /> à inverser avec le nouveau numéro
-			<input type="text" name="cafnum_user_new" class="type1" value="<?php if( isset($userTab['cafnum_user_new'])) echo $userTab['cafnum_user_new']; ?>" placeholder="" />
+			<input type="text" name="cafnum_user" class="type1" value="<?php echo $cafnumeUser; ?>" placeholder="" /> à inverser avec le nouveau numéro
+			<input type="text" name="cafnum_user_new" class="type1" value="<?php if($cafnumUserNew) echo $cafnumUserNew; ?>" placeholder="" />
 			<br />
 
 			<!--
@@ -144,7 +150,7 @@ if (!admin() && !allowed('user_edit_notme')) {
 
 			<br />
 			Qui peut le / la contacter sur le site, via un formulaire de contact (adresse e-mail jamais dévoilée) ?<br />
-			<?php $whocan_selected = $userTab['auth_contact_user']; ?>
+			<?php $whocan_selected = $authContactUser; ?>
             <?php $whocan_table = false; ?>
             <?php require __DIR__.'/../includes/user/whocan_contact.php'; ?>
 
