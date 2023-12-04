c4677a4cad00bff6edd0da20644ea7ef012a1f99
GaetanTrema <143398816+GaetanTrema@users.noreply.github.com>
2023-12-04 15:01:11 +0100

    Fix/expense report status as enum (#481)
    * add migration to change expense report status column type
    
    * fixes the values in Enum class

---
+++ migrations/Version20231204101515.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231204101515 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'changes the type of "status" colum in expense_report table to enum';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE expense_report CHANGE status status ENUM(\'submitted\', \'approved\', \'rejected\', \'draft\') NOT NULL');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE expense_report CHANGE status status VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
