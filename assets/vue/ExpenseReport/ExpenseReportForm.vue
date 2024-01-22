a5f75ad184fd82841e8febb768d3d6aec047e86b
broutous <gaetanbenaloul@gmail.com>
2024-01-22 14:32:02 +0100

    form validation

--- assets/vue/ExpenseReport/ExpenseReportForm.vue
+++ assets/vue/ExpenseReport/ExpenseReportForm.vue
@@ -93,7 +93,6 @@
             <div class="success" v-if="successMessage">
                 <p>{{ successMessage }}</p>
             </div>
-            <div class="errors" v-if="errorMessages.length"></div>
             <div class="buttons">
                 <button type="submit" class="biglink">
                     <span class="bleucaf">&gt;</span>
