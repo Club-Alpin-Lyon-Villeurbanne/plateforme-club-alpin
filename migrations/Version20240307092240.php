05f68ba7f60a068e64afaf862447e09492188439
fred-delaval-dupuis <38693470+fred-delaval-dupuis@users.noreply.github.com>
2024-03-08 11:20:18 +0100

    Migration générée pour synchroniser la db (#530)

---
+++ migrations/Version20240307092240.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240307092240 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX id_user ON caf_user (id_user)');
+        $this->addSql('ALTER TABLE sessions CHANGE sess_data sess_data LONGBLOB NOT NULL');
+        $this->addSql('ALTER TABLE sessions RENAME INDEX expiry TO sess_lifetime_idx');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX id_user ON caf_user');
+        $this->addSql('ALTER TABLE sessions CHANGE sess_data sess_data BLOB NOT NULL');
+        $this->addSql('ALTER TABLE sessions RENAME INDEX sess_lifetime_idx TO EXPIRY');
+    }
+}
