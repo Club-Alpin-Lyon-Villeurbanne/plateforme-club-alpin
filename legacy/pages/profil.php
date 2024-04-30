5b068755b1e3e9441f1d4f0c54f3a6dc9c6a0fb2
Kévin Liagre <kevin.liagre@gmail.com>
2024-04-30 08:55:38 +0200

    Simplify req.php (#585)
    * Move code related to research feature
    
    * Move code related to user profile
    
    * Move code related to admin partenaires
    
    * Move code related to outing validation
    
    * Move code related to outing deletion
    
    * Move code related to outing creation
    
    * Move code related to outing cancellation
    
    * Move code related to article display
    
    * Move code related to article management
    
    * Move code related to profile
    
    * Move code related to agenda
    
    * Move code related to outing management
    
    * ajout logic for adherent in own adherent page
    
    * fix formating
    
    * add allowed for adherent and fix admin-users
    
    ---------
    
    Co-authored-by: Nicolas Ritouet <nicolas@ritouet.com>

--- legacy/pages/profil.php
+++ legacy/pages/profil.php
@@ -8,6 +8,84 @@
 // 				aux filiations
 // 				...
 
+use App\Legacy\LegacyContainer;
+
+$MAX_ARTICLES_ADHERENT = LegacyContainer::getParameter('legacy_env_MAX_ARTICLES_ADHERENT');
+
+if ('infos' == $p2 && getUser()) {
+    $tmpUser = false;
+    $req = 'SELECT * FROM caf_user WHERE id_user='.getUser()->getId().' LIMIT 1';
+    $handleSql = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+    while ($handle = $handleSql->fetch_array(\MYSQLI_ASSOC)) {
+        // filiation : ais-je des "enfants"
+        if ('' !== trim($handle['cafnum_user'])) {
+            $handle['enfants'] = [];
+            $req = "SELECT id_user, firstname_user, lastname_user, nickname_user, birthday_user, email_user, tel_user, cafnum_user FROM caf_user WHERE cafnum_parent_user = '".LegacyContainer::get('legacy_mysqli_handler')->escapeString($handle['cafnum_user'])."' LIMIT 100";
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['enfants'][] = $handle2;
+            }
+        }
+
+        // filiation : ais-je un parent
+        if (isset($handle['cafnum_parent_user']) && '' !== trim($handle['cafnum_parent_user'])) {
+            $handle['parent'] = [];
+            $req = "SELECT id_user, firstname_user, lastname_user, nickname_user, birthday_user, email_user, tel_user, cafnum_user FROM caf_user WHERE cafnum_user = '".LegacyContainer::get('legacy_mysqli_handler')->escapeString($handle['cafnum_parent_user'])."' LIMIT 1";
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['parent'] = $handle2;
+            }
+        }
+
+        $tmpUser = $handle;
+    }
+} elseif ('articles' == $p2 && getUser()) {
+    // pagination
+    $limite = $MAX_ARTICLES_ADHERENT; // nombre d'elements affiches
+    $pagenum = (int) ($_GET['pagenum'] ?? 0);
+    if ($pagenum < 1) {
+        $pagenum = 1;
+    } // les pages commencent à 1
+
+    $articleTab = [];
+    $req = 'SELECT SQL_CALC_FOUND_ROWS * FROM caf_article
+			WHERE user_article = '.getUser()->getId().'
+			ORDER BY tsp_crea_article DESC LIMIT '.($limite * ($pagenum - 1)).", $limite";
+    $handleSql = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+
+    // calcul du total grâce à SQL_CALC_FOUND_ROWS
+    $totalSql = LegacyContainer::get('legacy_mysqli_handler')->query('SELECT FOUND_ROWS()');
+    $total = getArrayFirstValue($totalSql->fetch_array(\MYSQLI_NUM));
+    $nbrPages = ceil($total / $limite);
+
+    // boucle
+    while ($handle = $handleSql->fetch_array(\MYSQLI_ASSOC)) {
+        // info de la commission liée
+        if ($handle['commission_article'] > 0) {
+            $req = 'SELECT * FROM caf_commission
+				WHERE id_commission = '.(int) $handle['commission_article'].'
+				LIMIT 1';
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['commission'] = $handle2;
+            }
+        }
+
+        // info de la sortie liée
+        if ($handle['evt_article'] > 0) {
+            $req = 'SELECT code_evt, id_evt, titre_evt FROM caf_evt
+				WHERE id_evt = '.(int) $handle['evt_article'].'
+				LIMIT 1';
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['evt'] = $handle2;
+            }
+        }
+
+        $articleTab[] = $handle;
+    }
+}
+
 ?>
 
 <!-- MAIN -->
---
+++ legacy/scripts/get_sortie_params.php
@@ -0,0 +1,234 @@
+<?php
+
+use App\Legacy\LegacyContainer;
+
+$evt = false;
+$id_evt = null;
+$errPage = false; // message d'erreur spécifique à la page courante si besoin
+
+if ('feuille-de-sortie' == $p1) {
+    $type = strstr($p2, '-', true);
+    switch ($type) {
+        case 'evt':
+            $id_evt = (int) substr(strrchr($p2, '-'), 1);
+            break;
+        default:
+            break;
+    }
+} elseif ('sortie' == $p1) {
+    $id_evt = (int) substr(strrchr($p2, '-'), 1);
+}
+
+if ($id_evt) {
+    // selection complete, non conditionnelle par rapport au statut
+    $req = "SELECT
+            id_evt, code_evt, status_evt, status_legal_evt, status_who_evt, status_legal_who_evt,
+                user_evt, commission_evt, tsp_evt, tsp_end_evt, tsp_crea_evt, tsp_edit_evt, place_evt,
+                rdv_evt,titre_evt, massif_evt, tarif_evt, cycle_master_evt, cycle_parent_evt, child_version_from_evt,
+                cancelled_evt, cancelled_who_evt, cancelled_when_evt, description_evt, denivele_evt, difficulte_evt,
+                matos_evt, need_benevoles_evt, lat_evt, long_evt, join_start_evt, ngens_max_evt, join_max_evt,
+                id_groupe, tarif_detail, distance_evt, itineraire,
+            nickname_user, civ_user, firstname_user, lastname_user, tel_user,
+            title_commission, code_commission
+        FROM caf_evt as evt, caf_user as user, caf_commission as commission
+        WHERE id_evt=$id_evt
+            AND id_user = user_evt
+            AND commission_evt=commission.id_commission
+            LIMIT 1";
+
+    $handleSql = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+
+    while ($handle = $handleSql->fetch_array(\MYSQLI_ASSOC)) {
+        $on_peut_voir = true;
+
+        // on a le droit de voir cette page ?
+        if (
+            ($on_peut_voir && (1 == $handle['status_evt'])) // publiée
+            || (allowed('evt_validate') && isset($_GET['forceshow']) && $_GET['forceshow']) // ou mode validateur
+            || (allowed('evt_validate_all') && isset($_GET['forceshow']) &&  $_GET['forceshow']) // ou mode validateur
+            || (user() && $handle['user_evt'] == (string) getUser()->getId()) // ou j'en suis l'auteur ? QUID de l'encadrant ?
+        ) {
+            $current_commission = $handle['code_commission'];
+
+            // Groupe de niveau
+            $handle['groupe'] = [];
+            if (null != $handle['id_groupe']) {
+                $req = 'SELECT * FROM `caf_groupe` WHERE `id` = '.$handle['id_groupe'];
+                $handleGroupe = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                while ($groupe = $handleGroupe->fetch_array(\MYSQLI_ASSOC)) {
+                    $handle['groupe'] = $groupe;
+                }
+            }
+
+            // participants integres a la sortie
+            $handle['joins'] = ['inscrit' => [], 'manuel' => [], 'encadrant' => [], 'stagiaire' => [], 'coencadrant' => [], 'benevole' => [], 'enattente' => []];
+
+            if ($handle['cycle_parent_evt']) {
+                // cette sortie fait partie d'un cycle, alors on ajoute un lien vers son parent
+                $req = '
+                    SELECT id_evt, code_evt, status_evt, status_legal_evt, cancelled_evt, user_evt, commission_evt, tsp_evt, tsp_end_evt, tsp_crea_evt, tsp_edit_evt, place_evt, rdv_evt,titre_evt, massif_evt, tarif_evt, cycle_master_evt, cycle_parent_evt, child_version_from_evt, join_max_evt, join_start_evt
+                        , nickname_user, civ_user
+                        , title_commission, code_commission
+                    FROM caf_evt
+                        , caf_user
+                        , caf_commission
+                    WHERE id_user = user_evt
+                    AND id_evt='.(int) $handle['cycle_parent_evt'].'
+                    AND id_commission = commission_evt
+                    ORDER BY  `tsp_crea_evt` DESC
+                    LIMIT 1';
+
+                $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                    $handle['cycleparent'] = $handle2;
+                }
+            } elseif ($handle['cycle_master_evt']) {
+                // cette sortie est la premiere d'un cycle, on recupere les infos des sorties suivantes
+                $req = '
+                    SELECT id_evt, code_evt, status_evt, status_legal_evt, cancelled_evt, user_evt, commission_evt, title_commission, code_commission, tsp_evt, titre_evt, cycle_parent_evt
+                    FROM caf_evt
+                        , caf_commission
+                    WHERE cycle_parent_evt='.(int) $id_evt.'
+                    AND id_commission = commission_evt
+                    ORDER BY `tsp_crea_evt` ASC
+                    LIMIT 30';
+                $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                    $handle['cyclechildren'][] = $handle2;
+                }
+            }
+
+            // participants "speciaux" avec droits :
+            $req = "SELECT id_user, cafnum_user, firstname_user, lastname_user, nickname_user, nomade_user, tel_user, tel2_user, email_user, birthday_user, civ_user
+                        , role_evt_join, is_covoiturage
+                FROM caf_evt_join, caf_user
+                WHERE evt_evt_join = $id_evt
+                AND user_evt_join = id_user
+                AND status_evt_join = 1
+                AND
+                    (role_evt_join LIKE 'encadrant' OR role_evt_join LIKE 'stagiaire' OR role_evt_join LIKE 'coencadrant' OR role_evt_join LIKE 'benevole')
+                LIMIT 300";
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['joins'][$handle2['role_evt_join']][] = $handle2;
+            }
+
+            // participants "enattente" :
+            $req = 'SELECT DISTINCT id_user, cafnum_user, firstname_user, lastname_user, nickname_user, nomade_user, tel_user, tel2_user, email_user, birthday_user, civ_user
+                        , role_evt_join , is_covoiturage
+                FROM caf_evt_join, caf_user
+                WHERE evt_evt_join  = '.(int) ($handle['cycle_parent_evt'] ?: $id_evt).'
+                AND user_evt_join = id_user
+                AND status_evt_join = 0
+                LIMIT 300';
+
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['joins']['enattente'][] = $handle2;
+            }
+
+            // participants "normaux" : inscrit en ligne : leur role est à "inscrit"
+            $req = 'SELECT DISTINCT id_user, cafnum_user, firstname_user, lastname_user, nickname_user, nomade_user, tel_user, tel2_user, email_user, birthday_user, civ_user
+                        , role_evt_join, is_covoiturage
+                FROM caf_evt_join, caf_user
+                WHERE evt_evt_join  = '.(int) ($handle['cycle_parent_evt'] ?: $id_evt)."
+                AND user_evt_join = id_user
+                AND role_evt_join LIKE 'inscrit'
+                AND status_evt_join = 1
+                LIMIT 300";
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['joins']['inscrit'][] = $handle2;
+            }
+
+            // participants "manuel" : inscrit par l'orga : leur role est à "manuel"
+            $req = 'SELECT DISTINCT id_user, cafnum_user, firstname_user, lastname_user, nickname_user, nomade_user, tel_user, tel2_user, email_user, birthday_user, civ_user
+                        , role_evt_join, is_covoiturage
+                FROM caf_evt_join, caf_user
+                WHERE evt_evt_join  = '.(int) ($handle['cycle_parent_evt'] ?: $id_evt)."
+                AND user_evt_join = id_user
+                AND role_evt_join LIKE 'manuel'
+                AND status_evt_join = 1
+                LIMIT 300";
+            $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+            while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                $handle['joins']['manuel'][] = $handle2;
+            }
+
+            // mon rapport à cette sortie
+            $monStatut = 'neutre';
+
+            if (user()) {
+                $req = "SELECT * FROM caf_evt_join
+                    WHERE evt_evt_join=$id_evt
+                    AND user_evt_join=".getUser()->getId().'
+                    ORDER BY tsp_evt_join DESC
+                    LIMIT 1';
+                $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                    // si je suis pas encore validé
+                    if (0 == $handle2['status_evt_join']) {
+                        $monStatut = 'en attente';
+                    }
+                    // si je suis inscrit, "monStatut" prend la valeur de mon role
+                    if (1 == $handle2['status_evt_join']) {
+                        $monStatut = $handle2['role_evt_join'];
+                    }
+                    // si je suis refusé
+                    if (2 == $handle2['status_evt_join']) {
+                        $monStatut = 'refusé';
+                    }
+                }
+            }
+
+            if ('sortie' == $p1) {
+                // AUTRES INFOS, PAS NECESSAIRE POUR LA FICHE DE SORTIE
+
+                // si la sortie est annulée, on recupère les details de "WHO" : qui l'a annulée
+                if ('1' == $handle['cancelled_evt']) {
+                    $req = 'SELECT id_user, firstname_user, lastname_user, nickname_user, nomade_user, civ_user
+                        FROM caf_user
+                        WHERE id_user='.(int) $handle['cancelled_who_evt'].'
+                        LIMIT 300';
+                    $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                    while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                        $handle['cancelled_who_evt'] = $handle2;
+                    }
+                }
+
+                // si un compte rendu existe ?
+                $handle['cr'] = false;
+                $req = "SELECT id_article, titre_article, code_article
+                    FROM caf_article
+                    WHERE evt_article = $id_evt
+                    AND status_article = 1
+                    ORDER BY tsp_validate_article DESC
+                    LIMIT 1";
+                $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                    $handle['cr'] = $handle2;
+                }
+
+                // Modification des METAS de la page
+                $meta_title = $handle['titre_evt'].' | '.$p_sitename;
+                $meta_description = limiterTexte(strip_tags($handle['description_evt']), 200).'...';
+
+                // si je suis chef de famille (filiations) je rajoute la liste de mes "enfants" pour les inscrire
+                $filiations = [];
+                if (user() && getUser()->getCafnum()) {
+                    $req = "SELECT id_user, firstname_user, lastname_user, nickname_user, birthday_user, civ_user, email_user, tel_user, cafnum_user FROM caf_user WHERE cafnum_parent_user LIKE '".LegacyContainer::get('legacy_mysqli_handler')->escapeString(getUser()->getCafnum())."' LIMIT 15";
+                    $handleSql2 = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+                    while ($handle2 = $handleSql2->fetch_array(\MYSQLI_ASSOC)) {
+                        $filiations[] = $handle2;
+                    }
+                }
+            }
+
+            // go
+            $evt = $handle;
+        } else {
+            $errPage = 'Accès non autorisé';
+        }
+    }
+}
+
