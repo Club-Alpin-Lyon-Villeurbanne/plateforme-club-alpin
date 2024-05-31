95303fcbfb177775c58c79ef41196efddc3101cf
Kévin Liagre <kevin.liagre@gmail.com>
2024-06-01 01:31:41 +0200

    Replace the merge logic for duplicated new accounts (#614)

--- legacy/pages/adherents-modifier.php
+++ legacy/pages/adherents-modifier.php
@@ -7,26 +7,24 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 $operation = $_POST['operation'] ?? null;
 
 if (!admin() && !allowed('user_edit_notme')) {
-    echo 'Vos droits ne sont pas assez élevés pour accéder à cette page';
+	echo 'Vos droits ne sont pas assez élevés pour accéder à cette page';
 } else {
-    $idUser = (int) $_GET['id_user'];
-    if (!$idUser) {
-        echo 'Erreur : id invalide';
-        exit;
-    }
+	$idUser = (int) $_GET['id_user'];
+	if (!$idUser) {
+		echo 'Erreur : id invalide';
+		exit;
+	}
 
-    if (empty($userTab)) {
-        $req = "SELECT * FROM  `caf_user` WHERE id_user='".LegacyContainer::get('legacy_mysqli_handler')->escapeString($idUser)."' LIMIT 1";
-        $userTab = [];
-        $result = LegacyContainer::get('legacy_mysqli_handler')->query($req);
-        $userTab = $result->fetch_assoc();
+	$req = "SELECT * FROM  `caf_user` WHERE id_user='" . LegacyContainer::get('legacy_mysqli_handler')->escapeString($idUser) . "' LIMIT 1";
+	$userTab = [];
+	$result = LegacyContainer::get('legacy_mysqli_handler')->query($req);
+	$userTab = $result->fetch_assoc();
 
-        foreach ($userTab as $key => $val) {
-            $userTab[$key] = inputVal($key, $userTab[$key]);
-        }
-    }
-	
-	$civUser = $userTab['civ_user'];
+	foreach ($userTab as $key => $val) {
+		$userTab[$key] = inputVal($key, $userTab[$key]);
+	}
+
+	$civUser = $userTab['civ_user'] ?? '';
 	$firstnameUser = $userTab['firstname_user'];
 	$lastnameUser = $userTab['lastname_user'];
 	$nicknameUser = $userTab['nickname_user'];
@@ -44,8 +42,8 @@ if (!admin() && !allowed('user_edit_notme')) {
 	$cafnumUser = $userTab['cafnum_user'];
 	$authContactUser = $userTab['auth_contact_user'];
 	$userEmail = $userTab['email_user'];
-	
-	?>
+
+?>
 
 	<h1>Modifier un adhérent ou un salarié</h1>
 
@@ -63,51 +61,52 @@ if (!admin() && !allowed('user_edit_notme')) {
 
 		<?php
 
-        // TABLEAU
-        if ('user_edit' == $operation && isset($errTab) && count($errTab) > 0) {
-            echo '<div class="erreur">Erreur : <ul><li>'.implode('</li><li>', $errTab).'</li></ul></div>';
-        }
-    if ('user_edit' == $operation && (!isset($errTab) || 0 === count($errTab))) {
-        echo '<div class="info"><b>Adhérent modifié avec succès :</b> <ul><li>'.implode('</li><li>', $okTab).'</li></ul></div>';
-    } else {
-        ?>
-        <br />
+		// TABLEAU
+		if ('user_edit' == $operation && isset($errTab) && count($errTab) > 0) {
+			echo '<div class="erreur">Erreur : <ul><li>' . implode('</li><li>', $errTab) . '</li></ul></div>';
+		}
+		if ('user_edit' == $operation && (!isset($errTab) || 0 === count($errTab))) {
+			echo '<div class="info"><b>Adhérent modifié avec succès :</b> <ul><li>' . implode('</li><li>', $okTab) . '</li></ul></div>';
+		} else {
+		?>
+			<br />
 
 			<table>
 				<tr>
 					<td width='100px'><img src="<?php echo userImg($idUser, 'pic'); ?>" alt="" title="" style="max-width:100%" /></td>
-					<td><h1>
-						<?php
+					<td>
+						<h1>
+							<?php
 
-                            echo $civUser.' '.$firstnameUser.' '.$lastnameUser.'<br/>
-                            (<a href="/user-full/'.$idUser.'.html" title="Fiche profil" target="_top">'.$nicknameUser.'</a>)'; ?>
+							echo $civUser . ' ' . $firstnameUser . ' ' . $lastnameUser . '<br/>
+                            (<a href="/user-full/' . $idUser . '.html" title="Fiche profil" target="_top">' . $nicknameUser . '</a>)'; ?>
 						</h1>
 					</td>
 				</tr>
 			</table><br />
 
 			Date d'adhésion ou de renouvellement : <b>
-			<?php
-                // notification d'alerte si l'user doit renouveler sa licence
+				<?php
+				// notification d'alerte si l'user doit renouveler sa licence
 
-                if ($alerteRenouvelerUser) {
-                    echo '<span class="alerte">';
-                }
-        if ($dateAdhesionUser > 0) {
-            echo date('d/m/Y', $dateAdhesionUser);
-        } else {
-            echo 'aucune date connue.';
-        }
-        if ($alerteRenouvelerUser) {
-            echo '</span>';
-        } ?>
+				if ($alerteRenouvelerUser) {
+					echo '<span class="alerte">';
+				}
+				if ($dateAdhesionUser > 0) {
+					echo date('d/m/Y', $dateAdhesionUser);
+				} else {
+					echo 'aucune date connue.';
+				}
+				if ($alerteRenouvelerUser) {
+					echo '</span>';
+				} ?>
 			</b><br />
 
 			<?php
-                if (isset($validUser) && 1 != $validUser) {
-                    // compte non actif pour le moment (0 inactif, 1 actif)
-                    echo '<br />URL d\'activation du compte : '.LegacyContainer::get('legacy_router')->generate('legacy_root', [], UrlGeneratorInterface::ABSOLUTE_URL).'user-confirm/'.$userTab['cookietoken_user'].'-'.$idUser.'.html<br />';
-                } ?>
+			if (isset($validUser) && 1 != $validUser) {
+				// compte non actif pour le moment (0 inactif, 1 actif)
+				echo '<br />URL d\'activation du compte : ' . LegacyContainer::get('legacy_router')->generate('legacy_root', [], UrlGeneratorInterface::ABSOLUTE_URL) . 'user-confirm/' . $userTab['cookietoken_user'] . '-' . $idUser . '.html<br />';
+			} ?>
 
 			<br />
 			E-mail* :<br />
@@ -117,14 +116,14 @@ if (!admin() && !allowed('user_edit_notme')) {
 			Mot de passe :<br />
 			<input type="text" name="mdp_user" class="type1" value="" placeholder="" /> (pour ne pas le modifier, laisser vide)
 			<br />
-<!--
+			<!--
 			Confirmer le mot de passe* :<br />
 			<input type="password" name="mdp_user_confirm" class="type1" value="" placeholder="" />
 			<br />
  -->
 			Numéro de licence :<br />
-			<input type="text" name="cafnum_user" class="type1" value="<?php echo $cafnumUser; ?>" placeholder="" /> à inverser avec le nouveau numéro
-			<input type="text" name="cafnum_user_new" class="type1" value="<?php if($cafnumUserNew) echo $cafnumUserNew; ?>" placeholder="" />
+			<input type="text" name="cafnum_user" class="type1" value="<?php echo $cafnumUser; ?>" readonly /> à inverser avec le nouveau numéro
+			<input type="text" name="cafnum_user_new" class="type1" value="<?php if ($cafnumUserNew) echo $cafnumUserNew; ?>" placeholder="" />
 			<br />
 
 			<!--
@@ -154,15 +153,15 @@ if (!admin() && !allowed('user_edit_notme')) {
 			<br />
 			Qui peut le / la contacter sur le site, via un formulaire de contact (adresse e-mail jamais dévoilée) ?<br />
 			<?php $whocan_selected = $authContactUser; ?>
-            <?php $whocan_table = false; ?>
-            <?php require __DIR__.'/../includes/user/whocan_contact.php'; ?>
+			<?php $whocan_table = false; ?>
+			<?php require __DIR__ . '/../includes/user/whocan_contact.php'; ?>
 
 			<a href="javascript:void(0)" title="Enregistrer" onclick="$(this).parents('form').submit()">
 				<img src="/img/base/save.png" alt="Enregistrer" title="Enregistrer" style="height:35px;" />
 				Enregistrer
 			</a>
-			<?php
-    } ?>
+		<?php
+		} ?>
 	</form>
 
 <?php
--- legacy/pages/admin-users.php
+++ legacy/pages/admin-users.php
@@ -3,7 +3,7 @@
 use App\Legacy\LegacyContainer;
 
 if (!admin()) {
-    echo 'Votre session administrateur a expiré';
+	echo 'Votre session administrateur a expiré';
 } else {
 	$userTab = [];
 	$show = 'valid';
@@ -14,16 +14,15 @@ if (!admin()) {
 	$show = LegacyContainer::get('legacy_mysqli_handler')->escapeString($show);
 
 	$req = 'SELECT id_user , email_user , cafnum_user , firstname_user , lastname_user , nickname_user , created_user , birthday_user , tel_user , tel2_user , adresse_user, cp_user ,  ville_user ,  civ_user , valid_user , manuel_user, nomade_user, date_adhesion_user, doit_renouveler_user
-		FROM  `caf_user` '
-		.('dels' == $show ? ' WHERE valid_user=2 ' : '')
-		.('manual' == $show ? ' WHERE manuel_user=1 ' : '')
-		.('nomade' == $show ? ' WHERE nomade_user=1 ' : '')
-		.('valid' == $show ? ' WHERE valid_user=1 AND doit_renouveler_user=0 AND nomade_user=0 ' : '')
-		.('notvalid' == $show ? ' WHERE valid_user=0 AND doit_renouveler_user=0 AND nomade_user=0 ' : '')
-		.('expired' == $show ? ' WHERE valid_user=0 AND doit_renouveler_user=1 ' : '')
-		.('valid-expired' == $show ? ' WHERE valid_user=1 AND doit_renouveler_user=1 ' : '')
-		.' ORDER BY lastname_user ASC, lastname_user ASC
-		LIMIT 9000';			// , pays_user
+		FROM  `caf_user` WHERE is_deleted=0'
+		. ('dels' == $show ? ' AND valid_user=2 ' : '')
+		. ('manual' == $show ? ' AND manuel_user=1 ' : '')
+		. ('nomade' == $show ? ' AND nomade_user=1 ' : '')
+		. ('valid' == $show ? ' AND valid_user=1 AND doit_renouveler_user=0 AND nomade_user=0 ' : '')
+		. ('notvalid' == $show ? ' AND valid_user=0 AND doit_renouveler_user=0 AND nomade_user=0 ' : '')
+		. ('expired' == $show ? ' AND valid_user=0 AND doit_renouveler_user=1 ' : '')
+		. ('valid-expired' == $show ? ' AND valid_user=1 AND doit_renouveler_user=1 ' : '')
+		. ' ORDER BY lastname_user ASC, lastname_user ASC LIMIT 9000';			// , pays_user
 
 	$handleSql = LegacyContainer::get('legacy_mysqli_handler')->query($req);
 	while ($row = $handleSql->fetch_assoc()) {
@@ -36,7 +35,7 @@ if (!admin()) {
 
 		$userTab[] = $row;
 	}
-    ?>
+?>
 	<h1>Administration des utilisateurs & adhérents</h1>
 	<p>
 		<img src="/img/base/magnifier.png" style="vertical-align:middle" />
@@ -50,29 +49,29 @@ if (!admin()) {
 
 	<p><strong>Voir les comptes adhérents :</strong>
 		<a href="/admin-users.html" <?php if ('valid' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> activé / licence valide </a>&nbsp;
+										echo 'style="background:#d3d6ff"';
+									} ?> class="boutonFancy"> activé / licence valide </a>&nbsp;
 		<a href="/admin-users.html?show=valid-expired" <?php if ('valid-expired' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> activé / licence expirée </a>&nbsp;
+															echo 'style="background:#d3d6ff"';
+														} ?> class="boutonFancy"> activé / licence expirée </a>&nbsp;
 		<a href="/admin-users.html?show=notvalid" <?php if ('notvalid' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> non activé / licence valide </a>&nbsp;
+														echo 'style="background:#d3d6ff"';
+													} ?> class="boutonFancy"> non activé / licence valide </a>&nbsp;
 		<a href="/admin-users.html?show=expired" <?php if ('expired' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> non activé / licence expirée </a>&nbsp;
+														echo 'style="background:#d3d6ff"';
+													} ?> class="boutonFancy"> non activé / licence expirée </a>&nbsp;
 		<a href="/admin-users.html?show=dels" <?php if ('dels' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> désactivés manuellement </a>&nbsp;
+													echo 'style="background:#d3d6ff"';
+												} ?> class="boutonFancy"> désactivés manuellement </a>&nbsp;
 		<a href="/admin-users.html?show=manual" <?php if ('manual' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> créés manuellement </a>&nbsp;
+													echo 'style="background:#d3d6ff"';
+												} ?> class="boutonFancy"> créés manuellement </a>&nbsp;
 		<a href="/admin-users.html?show=nomade" <?php if ('nomade' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> nomades </a>&nbsp;
+													echo 'style="background:#d3d6ff"';
+												} ?> class="boutonFancy"> nomades </a>&nbsp;
 		<a href="/admin-users.html?show=all" <?php if ('all' == $show) {
-		    echo 'style="background:#d3d6ff"';
-		} ?> class="boutonFancy"> tous (+long) </a>
+													echo 'style="background:#d3d6ff"';
+												} ?> class="boutonFancy"> tous (+long) </a>
 	</p>
 
 
@@ -83,28 +82,31 @@ if (!admin()) {
 	<script type="text/javascript" src="/tools/datatables/extras/TableTools/media/js/TableTools.min.js"></script>
 
 	<script type="text/javascript">
-	$(document).ready(function() {
-		$('#pagesLibres').dataTable( {
-			"iDisplayLength": 100,
-			"aaSorting": [[ 2, "desc" ], [ 4, "asc" ]],
-			"sDom": 'T<"clear">lfrtip',
-			"oTableTools": {
-				"sSwfPath": "/tools/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf",
-				"aButtons": [
-					"copy",
-					"csv",
-					"xls",
-					{
-						"sExtends": "pdf",
-						"sPdfOrientation": "landscape"
-						// "sPdfMessage": "Your custom message would go here."
-					},
-					"print"
-				]
-			}
-		} );
-		$('span.br').html('<br />');
-	});
+		$(document).ready(function() {
+			$('#pagesLibres').dataTable({
+				"iDisplayLength": 100,
+				"aaSorting": [
+					[2, "desc"],
+					[4, "asc"]
+				],
+				"sDom": 'T<"clear">lfrtip',
+				"oTableTools": {
+					"sSwfPath": "/tools/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf",
+					"aButtons": [
+						"copy",
+						"csv",
+						"xls",
+						{
+							"sExtends": "pdf",
+							"sPdfOrientation": "landscape"
+							// "sPdfMessage": "Your custom message would go here."
+						},
+						"print"
+					]
+				}
+			});
+			$('span.br').html('<br />');
+		});
 	</script>
 
 
@@ -133,74 +135,74 @@ if (!admin()) {
 		</thead>
 		<tbody>
 			<?php
-		        $total = 0;
+			$total = 0;
 
-    for ($i = 0; $i < count($userTab); ++$i) {
-        $elt = $userTab[$i];
+			for ($i = 0; $i < count($userTab); ++$i) {
+				$elt = $userTab[$i];
 
-        echo '<tr id="tr-'.$elt['id_user'].'" class="'.($elt['valid_user'] ? 'vis-on' : 'vis-off').'">'
-                    .'<td style="white-space:nowrap;">'
+				echo '<tr id="tr-' . $elt['id_user'] . '" class="' . ($elt['valid_user'] ? 'vis-on' : 'vis-off') . '">'
+					. '<td style="white-space:nowrap;">'
 
-                        // view user
-                        .'<a href="/includer.php?p=pages/adherents-consulter.php&amp;id_user='.(int) $elt['id_user'].'" class="fancyframe" title="Consulter cet adhérent"><img src="/img/base/report.png"></a> '
+					// view user
+					. '<a href="/includer.php?p=pages/adherents-consulter.php&amp;id_user=' . (int) $elt['id_user'] . '" class="fancyframe" title="Consulter cet adhérent"><img src="/img/base/report.png"></a> '
 
-                        // gestion des droits
-                        .'<a href="/includer.php?admin=true&amp;p=pages/admin-users-droits.php&amp;id_user='.(int) $elt['id_user'].'&amp;nom='.urlencode($elt['civ_user'].' '.$elt['firstname_user'].' '.$elt['lastname_user']).'" class="fancyframe" title="Voir / Attribuer des statuts à cet utilisateur"><img src="/img/base/user_star.png"></a> ';
+					// gestion des droits
+					. '<a href="/includer.php?admin=true&amp;p=pages/admin-users-droits.php&amp;id_user=' . (int) $elt['id_user'] . '&amp;nom=' . urlencode($elt['civ_user'] . ' ' . $elt['firstname_user'] . ' ' . $elt['lastname_user']) . '" class="fancyframe" title="Voir / Attribuer des statuts à cet utilisateur"><img src="/img/base/user_star.png"></a> ';
 
-        // désactiver
-        if (allowed('user_desactivate_any') && '1' == $elt['valid_user']) {
-            echo '<a href="/includer.php?p=pages/adherents-desactiver.php&amp;id_user='.(int) $elt['id_user'].'&amp;nom='.urlencode($elt['civ_user'].' '.$elt['firstname_user'].' '.$elt['lastname_user']).'" class="fancyframe" title="Désactiver le compte de cet utilisateur"><img src="/img/base/user_unvalidate.png"></a> ';
-        }
+				// désactiver
+				if (allowed('user_desactivate_any') && '1' == $elt['valid_user']) {
+					echo '<a href="/includer.php?p=pages/adherents-desactiver.php&amp;id_user=' . (int) $elt['id_user'] . '&amp;nom=' . urlencode($elt['civ_user'] . ' ' . $elt['firstname_user'] . ' ' . $elt['lastname_user']) . '" class="fancyframe" title="Désactiver le compte de cet utilisateur"><img src="/img/base/user_unvalidate.png"></a> ';
+				}
 
-        // réactiver
-        if (allowed('user_reactivate') && '2' == $elt['valid_user']) {
-            echo '<a href="/includer.php?p=pages/adherents-reactiver.php&amp;id_user='.(int) $elt['id_user'].'&amp;nom='.urlencode($elt['civ_user'].' '.$elt['firstname_user'].' '.$elt['lastname_user']).'" class="fancyframe" title="Réactiver le compte de cet utilisateur"><img src="/img/base/user_revalidate.png"></a> ';
-        }
+				// réactiver
+				if (allowed('user_reactivate') && '2' == $elt['valid_user']) {
+					echo '<a href="/includer.php?p=pages/adherents-reactiver.php&amp;id_user=' . (int) $elt['id_user'] . '&amp;nom=' . urlencode($elt['civ_user'] . ' ' . $elt['firstname_user'] . ' ' . $elt['lastname_user']) . '" class="fancyframe" title="Réactiver le compte de cet utilisateur"><img src="/img/base/user_revalidate.png"></a> ';
+				}
 
-        // reset user
-        if (allowed('user_reset')) {
-            echo '<a href="/includer.php?p=pages/adherents-reset.php&amp;id_user='.(int) $elt['id_user'].'&amp;nom='.urlencode($elt['civ_user'].' '.$elt['firstname_user'].' '.$elt['lastname_user']).'" class="fancyframe" title="Remettre à zéro, réinitialiser le compte de cet utilisateur"><img src="/img/base/user_reset.png"></a> ';
-        }
+				// reset user
+				if (allowed('user_reset')) {
+					echo '<a href="/includer.php?p=pages/adherents-reset.php&amp;id_user=' . (int) $elt['id_user'] . '&amp;nom=' . urlencode($elt['civ_user'] . ' ' . $elt['firstname_user'] . ' ' . $elt['lastname_user']) . '" class="fancyframe" title="Remettre à zéro, réinitialiser le compte de cet utilisateur"><img src="/img/base/user_reset.png"></a> ';
+				}
 
-        // edit user
-        if (allowed('user_edit_notme')) {
-            echo '<a href="/includer.php?p=pages/adherents-modifier.php&amp;id_user='.(int) $elt['id_user'].'" class="fancyframe" title="Modifier cet adhérent"><img src="/img/base/user_edit.png"></a> ';
-        }
+				// edit user
+				if (allowed('user_edit_notme')) {
+					echo '<a href="/includer.php?p=pages/adherents-modifier.php&amp;id_user=' . (int) $elt['id_user'] . '" class="fancyframe" title="Modifier cet adhérent"><img src="/img/base/user_edit.png"></a> ';
+				}
 
-        if (isGranted('ROLE_ALLOWED_TO_SWITCH')) {
-            echo (1 == $elt['valid_user'] && $elt['email_user']) ? ' <a href="/profil.html?_switch_user='.urlencode($elt['email_user']).'" title="Impersonifier l\'utilisateur"><img src="/img/base/user_go.png"></a> ' : ' ';
-        }
+				if (isGranted('ROLE_ALLOWED_TO_SWITCH')) {
+					echo (1 == $elt['valid_user'] && $elt['email_user']) ? ' <a href="/profil.html?_switch_user=' . urlencode($elt['email_user']) . '" title="Impersonifier l\'utilisateur"><img src="/img/base/user_go.png"></a> ' : ' ';
+				}
 
-        echo '</td>'
-                    .'<td>'
-                        .html_utf8($elt['cafnum_user']).'<br />'
-                        .($elt['manuel_user'] ? '<img src="/img/base/user_manuel.png" alt="MANUEL" title="Utilisateur créé manuellement" /> ' : '')
-                        .($elt['nomade_user'] ? '<img src="/img/base/nomade_user.png" alt="NOMADE" title="Utilisateur nomade" /> ' : '')
-                        .('2' == $elt['valid_user'] ? '<img src="/img/base/user_desactive.png" alt="DESACTIVE" title="Utilisateur désactivé manuellement" /> ' : '')
-                        .(int) $elt['id_user'].' '
-                    .'</td>'
-                    .'<td>'.(int) $elt['valid_user'].'</td>'
-                    .'<td>'.html_utf8($elt['civ_user']).'</td>'
-                    .'<td>'.html_utf8($elt['lastname_user']).'</td>'
-                    .'<td>'.html_utf8($elt['firstname_user']).'</td>'
-                    .'<td>'.($elt['date_adhesion_user'] ? date('Y-m-d', $elt['date_adhesion_user']) : '-').'</td>'
-                    .'<td>'.userlink($elt['id_user'], $elt['nickname_user']).'</td>'
-                    .'<td><span style="display:none">'.$elt['birthday_user'].'</span>'.($elt['birthday_user'] ? (int) ($elt['birthday_user']).' ans' : '...').'</td>'
-                    .'<td>'.html_utf8($elt['tel_user']).'<br />'.html_utf8($elt['tel2_user']).'</td>'
-                    .'<td><a href="mailto:'.html_utf8($elt['email_user']).'" title="Contact direct">'.html_utf8($elt['email_user']).'</a></td>'
-                    // .'<td>'.nl2br(html_utf8($elt['adresse_user'])).'</td>'
-                    .'<td>'.html_utf8($elt['cp_user']).'</td>'
-                    .'<td>'.html_utf8($elt['ville_user']).'</td>'
-                    // .'<td>'.html_utf8($elt['pays_user']).'</td>'
-                    .'<td>'.($elt['doit_renouveler_user'] ? 'expirée' : 'valide').' '.(!$elt['doit_renouveler_user'] && isset($elt['alerte_renouveler_user']) && $elt['alerte_renouveler_user'] ? '<span style="color:red">* Doit renouveler</span>' : '').'</td>'
-                    // .'<td></td>'
-                .'</tr>';
-    } ?>
+				echo '</td>'
+					. '<td>'
+					. html_utf8($elt['cafnum_user']) . '<br />'
+					. ($elt['manuel_user'] ? '<img src="/img/base/user_manuel.png" alt="MANUEL" title="Utilisateur créé manuellement" /> ' : '')
+					. ($elt['nomade_user'] ? '<img src="/img/base/nomade_user.png" alt="NOMADE" title="Utilisateur nomade" /> ' : '')
+					. ('2' == $elt['valid_user'] ? '<img src="/img/base/user_desactive.png" alt="DESACTIVE" title="Utilisateur désactivé manuellement" /> ' : '')
+					. (int) $elt['id_user'] . ' '
+					. '</td>'
+					. '<td>' . (int) $elt['valid_user'] . '</td>'
+					. '<td>' . html_utf8($elt['civ_user']) . '</td>'
+					. '<td>' . html_utf8($elt['lastname_user']) . '</td>'
+					. '<td>' . html_utf8($elt['firstname_user']) . '</td>'
+					. '<td>' . ($elt['date_adhesion_user'] ? date('Y-m-d', $elt['date_adhesion_user']) : '-') . '</td>'
+					. '<td>' . userlink($elt['id_user'], $elt['nickname_user']) . '</td>'
+					. '<td><span style="display:none">' . $elt['birthday_user'] . '</span>' . ($elt['birthday_user'] ? (int) ($elt['birthday_user']) . ' ans' : '...') . '</td>'
+					. '<td>' . html_utf8($elt['tel_user']) . '<br />' . html_utf8($elt['tel2_user']) . '</td>'
+					. '<td><a href="mailto:' . html_utf8($elt['email_user']) . '" title="Contact direct">' . html_utf8($elt['email_user']) . '</a></td>'
+					// .'<td>'.nl2br(html_utf8($elt['adresse_user'])).'</td>'
+					. '<td>' . html_utf8($elt['cp_user']) . '</td>'
+					. '<td>' . html_utf8($elt['ville_user']) . '</td>'
+					// .'<td>'.html_utf8($elt['pays_user']).'</td>'
+					. '<td>' . ($elt['doit_renouveler_user'] ? 'expirée' : 'valide') . ' ' . (!$elt['doit_renouveler_user'] && isset($elt['alerte_renouveler_user']) && $elt['alerte_renouveler_user'] ? '<span style="color:red">* Doit renouveler</span>' : '') . '</td>'
+					// .'<td></td>'
+					. '</tr>';
+			} ?>
 		</tbody>
 	</table>
 
 	<br style="clear:both" />
 	<br style="clear:both" />
-	<?php
+<?php
 }
 ?>
