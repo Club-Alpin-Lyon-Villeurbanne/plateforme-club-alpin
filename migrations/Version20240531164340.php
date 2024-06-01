e9917a2bd5589b4997ba963c6c972777a9caa74f
Kévin Liagre <kevin.liagre@gmail.com>
2024-06-01 15:24:15 +0200

    Remove unique constraint on cafnum to fix failing migration (#615)

--- migrations/Version20240531164340.php
+++ migrations/Version20240531164340.php
@@ -20,12 +20,10 @@ final class Version20240531164340 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE caf_user ADD is_deleted TINYINT(1) DEFAULT 0 NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_DEBE82686A22D67B ON caf_user (cafnum_user)');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP INDEX UNIQ_DEBE82686A22D67B ON caf_user');
         $this->addSql('ALTER TABLE caf_user DROP is_deleted');
     }
 }
