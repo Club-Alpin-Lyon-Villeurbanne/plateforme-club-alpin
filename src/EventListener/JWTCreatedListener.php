1cd43d683e39b085102054c2f59b875c41fa9ef4
Kévin Liagre <kevin.liagre@gmail.com>
2024-06-04 13:20:40 +0200

    Add JWT Token in HTML Page content for async calls (#612)

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Security\AdminDetector;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class JWTCreatedListener implements EventSubscriberInterface
+{
+    private $adminDetector;
+
+    public function __construct(AdminDetector $adminDetector)
+    {
+        $this->adminDetector = $adminDetector;
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        $isAdmin = $this->adminDetector->isAdmin();
+        if ($isAdmin) {
+            $payload['is_admin'] = $isAdmin;
+        }
+
+        $event->setData($payload);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::JWT_CREATED => 'onJWTCreated',
+        ];
+    }
+}
