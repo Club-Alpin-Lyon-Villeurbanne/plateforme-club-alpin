b67ad3302a7d62538e1d45808ec85b46723e40c4
Nicolas Ritouet <nicolas@ritouet.com>
2024-06-21 19:10:58 +0200

    Cleanup

--- assets/vue/ExpenseReport/ExpenseField.vue
+++ assets/vue/ExpenseReport/ExpenseField.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="field" :class="{error: field.errors}">
-        <label>{{ field.name }} {{ tauxKilometrique }} {{ field.flags.isMandatory ? '*' : '' }}</label>
+        <label>{{ field.name }} {{ field.flags.isMandatory ? '*' : '' }}</label>
 
         <input
             :required="field.flags.isMandatory"
@@ -82,17 +82,6 @@ export default defineComponent({
     data: () => ({
         justificationFileUrl: '',
     }),
-    computed: {
-       tauxKilometrique: function() {
-            if (this.expenseType === 'vehicule_personnel' && this.field.slug === 'distance') {
-                return '(Taux kilométrique: ' + parseFloat(this.config.tauxKilometriqueVoiture) + '€/km)';
-            } else if (this.expenseType === 'minibus_club' && this.field.slug === 'distance') {
-                return '(Taux kilométrique: ' + parseFloat(this.config.tauxKilometriqueMinibus) + '€/km)';
-            } else {
-                return '';
-            }
-       }
-    },
     methods: {
         onFileUploadChange(event: any) {
             this.field.justificationFile = event.target.files[0];
