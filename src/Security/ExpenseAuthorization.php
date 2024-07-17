40ddc3f8c30d1f605fded6c636dfe786e3b030c7
Kévin Liagre <kevin.liagre@gmail.com>
2024-07-17 22:31:30 +0200

    Use environment vars to authorize users for expenses

--- src/Security/ExpenseAuthorization.php
+++ src/Security/ExpenseAuthorization.php
@@ -6,38 +6,17 @@ use Symfony\Bundle\SecurityBundle\Security;
 
 class ExpenseAuthorization
 {
-    private $gistUrl;
     private $authorizedIds;
     private $security;
 
-    public function __construct(string $gistUrl, Security $security)
+    public function __construct(string $authorizedIds, Security $security)
     {
-        $this->gistUrl = $gistUrl;
+        $this->authorizedIds = explode(',', $authorizedIds);
         $this->security = $security;
-    }
-
-    private function fetchAuthorizedIds(): void
-    {
-        $ch = curl_init();
-        curl_setopt($ch, \CURLOPT_URL, $this->gistUrl);
-        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, \CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)');
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        if (false !== $response) {
-            $jsonResponse = json_decode($response, true);
-
-            $this->authorizedIds = array_map('intval', explode(',', array_values($jsonResponse['files'])[0]['content']));
-        } else {
-            $this->authorizedIds = [];
-        }
     }
 
     public function isAuthorized(): bool
     {
-        $this->fetchAuthorizedIds();
-
         $user = $this->security->getUser();
 
         if (null === $user) {
