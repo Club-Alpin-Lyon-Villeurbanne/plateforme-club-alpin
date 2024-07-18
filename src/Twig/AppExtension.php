699ffbb16d742170e465272558fb0a7866484b49
fred-delaval-dupuis <38693470+fred-delaval-dupuis@users.noreply.github.com>
2024-07-18 09:46:48 +0200

    User profile warning (#638)
    * Validates a user profile and displays a warning message in a banner
    
    * Warning message updated and moved to an alert message in user menu

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Utils\User\UserProfileValidator;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly UserProfileValidator $userProfileValidator
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('validate_user_profile', [$this, 'validateUserProfile']),
+            new TwigFunction('is_user_profile_incomplete', [$this, 'isUserProfileIncomplete']),
+            new TwigFunction('display_user_profile_warning', [$this, 'displayUserProfileWarning'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function validateUserProfile(): array
+    {
+        return $this->userProfileValidator->validateUserProfile();
+    }
+
+    public function isUserProfileIncomplete(): bool
+    {
+        return $this->userProfileValidator->isUserProfileIncomplete();
+    }
+
+    public function displayUserProfileWarning(): string
+    {
+        return $this->validateUserProfile()['message'];
+    }
+}
