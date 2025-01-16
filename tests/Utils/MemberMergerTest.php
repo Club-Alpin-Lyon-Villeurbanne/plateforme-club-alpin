b6d396ee74c15f9e81bed7e50ed527dc477555b0
Kévin Liagre <kevin.liagre@gmail.com>
2025-01-16 20:41:26 +0100

    Enhance FFCAM synchronization to automatically merge same members (#851)

--- tests/Utils/MemberMergerTest.php
+++ tests/Utils/MemberMergerTest.php
@@ -8,7 +8,7 @@ use App\Utils\MemberMerger;
 
 class MemberMergerTest extends WebTestCase
 {
-    public function testMergeMembers(): void
+    public function testMergeExistingMembers(): void
     {
         $entityManager = static::getContainer()->get('doctrine')->getManager();
         $memberMerger = static::getContainer()->get(MemberMerger::class);
@@ -19,12 +19,38 @@ class MemberMergerTest extends WebTestCase
         $oldLicense = $user1->getCafnum();
         $newLicense = $user2->getCafnum();
 
-        $memberMerger->mergeMembers($oldLicense, $newLicense);
+        $memberMerger->mergeExistingMembers($oldLicense, $newLicense);
 
         $userOldLicense = $entityManager->getRepository(User::class)->findOneByLicenseNumber("obs_{$newLicense}");
         $userNewLicense = $entityManager->getRepository(User::class)->findOneByLicenseNumber($newLicense);
 
         $this->assertSame($userNewLicense->getId(), $user1->getId());
         $this->assertSame($userOldLicense->getId(), $user2->getId());
+    }
+
+    public function testMergeNewMember(): void
+    {
+        $entityManager = static::getContainer()->get('doctrine')->getManager();
+        $memberMerger = static::getContainer()->get(MemberMerger::class);
+
+        $user1 = $this->signup();
+        $user2 = new User();
+
+        $user2Cafnum = mt_rand(100000000000, 999999999999);
+        $user2->setEmail('test-' . bin2hex(random_bytes(12)) . '@clubalpinlyon.fr')
+            ->setCafnum($user2Cafnum)
+            ->setFirstname('prenom')
+            ->setLastname('nom')
+            ->setDoitRenouveler(false)
+            ->setAlerteRenouveler(false);
+
+        $oldLicense = $user1->getCafnum();
+
+        $memberMerger->mergeNewMember($oldLicense, $user2);
+
+        $userOldLicense = $entityManager->getRepository(User::class)->findOneByLicenseNumber("obs_{$user2Cafnum}");
+        $userNewLicense = $entityManager->getRepository(User::class)->findOneByLicenseNumber($user2Cafnum);
+
+        $this->assertSame($userNewLicense->getId(), $user1->getId());
     }
 }
