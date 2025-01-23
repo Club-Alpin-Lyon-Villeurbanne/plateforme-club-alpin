0003a8abe37f08aa72417cf0e0d3d9e43b8c871d
Nicolas Ritouet <nicolas@ritouet.com>
2025-01-23 04:43:29 +0100

    NDF: Ajout aide sous forme de tooltip

--- assets/expense-report-form/src/components/OtherSection.vue
+++ assets/expense-report-form/src/components/OtherSection.vue
@@ -5,7 +5,7 @@
         Autres dépenses
       </h2>
       <InfoTooltip
-        text="Saisissez les dépenses que vous souhaitez vous faire rembourser autre que le transport et l'hébergement."
+        text="Saisissez les dépenses que vous souhaitez vous faire rembourser autres que le transport et l'hébergement."
         ariaLabel="Informations sur les autres dépenses"
       />
     </div>
