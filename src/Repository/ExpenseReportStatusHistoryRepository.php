ea6c10642bce9c3aee76e5a17e3af20fe077319f
Nicolas Ritouet <nicolas@ritouet.com>
2025-04-21 21:49:59 +0200

    clean code comment

--- src/Repository/ExpenseReportStatusHistoryRepository.php
+++ src/Repository/ExpenseReportStatusHistoryRepository.php
@@ -15,29 +15,4 @@ class ExpenseReportStatusHistoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ExpenseReportStatusHistory::class);
     }
-
-//    /**
-//     * @return ExpenseReportStatusHistory[] Returns an array of ExpenseReportStatusHistory objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?ExpenseReportStatusHistory
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
