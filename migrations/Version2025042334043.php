ed0850ea3cb005fc7b4f61817dd69f8f1abd5355
Nicolas Ritouet <nicolas@ritouet.com>
2025-04-23 10:23:53 +0200

    fix errors

--- migrations/Version2025042334043.php
+++ migrations/Version2025042334043.php
@@ -1,17 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-return new class extends AbstractMigration
+final class Version2025042334043 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return 'Add details_caches_evt column to caf_evt table';
-    }
-
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE caf_evt ADD details_caches_evt TEXT DEFAULT NULL');
@@ -21,4 +18,4 @@ return new class extends AbstractMigration
     {
         $this->addSql('ALTER TABLE caf_evt DROP details_caches_evt');
     }
-}; 
+}
