d8bd9ea57ce2efde33933ebe7cb97cb2c531274d
ENERGEO-FB <fbaccam@energeotechnologies.com>
2025-04-24 08:36:41 +0200

    Feat: Ajout d'un champ de détails de sortie caché au grand public (#844)
    * Add new visio field in event form
    
    * Fix php-cs
    
    * Fix php-cs
    
    * changement de spec: plus de visio, mais détails cachés
    
    * fix errors
    
    * suppression detail label cachés
    
    * fix namespace
    
    * corrections finales
    
    * fix migration file
    
    * remove useless length property
    
    * fix ident
    
    ---------
    
    Co-authored-by: Nicolas Ritouet <nicolas@ritouet.com>

---
+++ migrations/Version20250423340438.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250423340438 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt ADD details_caches_evt TEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt DROP details_caches_evt');
+    }
+}
