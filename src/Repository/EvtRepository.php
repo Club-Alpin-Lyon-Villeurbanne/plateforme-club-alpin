63f23ae6a229bd9a22232925497e8acae3628080
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-04-29 11:54:19 +0200

    fix: bloc agenda : ne pas afficher toutes les sorties passées (#932)

--- src/Repository/EvtRepository.php
+++ src/Repository/EvtRepository.php
@@ -74,7 +74,7 @@ class EvtRepository extends ServiceEntityRepository
             ->leftJoin('e.commission', 'c')
             ->where('e.status = :status')
             ->setParameter('status', Evt::STATUS_LEGAL_VALIDE)
-            ->andWhere(':date >= e.tsp OR :date <= e.tspEnd')
+            ->andWhere(':date <= e.tsp OR :date <= e.tspEnd')
             ->setParameter('date', $date->getTimestamp())
             ->orderBy('e.tsp', 'asc')
             ->addOrderBy('c.title', 'ASC')
