ecdadd42fe91a2694ca3ccee11290b17f998cf74
Nicolas Ritouet <nicolas@ritouet.com>
2025-05-06 18:00:01 +0200

    NDF: fix note de frais non trouvée

--- src/Controller/Api/ExpenseAttachmentController.php
+++ src/Controller/Api/ExpenseAttachmentController.php
@@ -36,7 +36,7 @@ class ExpenseAttachmentController extends AbstractController
 
         $body = $request->getPayload()->all();
 
-        $expenseReport = $this->expenseReportRepository->findOneBy(['id' => $expenseReportId, 'user' => $user]);
+        $expenseReport = $this->expenseReportRepository->getExpenseReportByEventAndUser($expenseReportId, $user);
         if (!$expenseReport) {
             throw $this->createNotFoundException('ExpenseReport not found');
         }
