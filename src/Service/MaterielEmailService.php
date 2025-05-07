17f865f5243057810b720bb7d272019d07e4d4eb
Nicolas Ritouet <nicolas@ritouet.com>
2025-05-07 18:42:19 +0200

    utiliser de la classe Mailer

--- src/Service/MaterielEmailService.php
+++ src/Service/MaterielEmailService.php
@@ -2,33 +2,29 @@
 
 namespace App\Service;
 
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
+use App\Mailer\Mailer;
 
 class MaterielEmailService
 {
     public function __construct(
-        private readonly MailerInterface $mailer,
+        private readonly Mailer $mailer,
         private readonly string $materielPlatformUrl
     ) {
     }
 
     public function sendAccountCreationEmail(string $to, string $firstName, string $lastName, array $credentials): void
     {
-        $email = (new TemplatedEmail())
-            ->from('materiel@clubalpinlyon.fr')
-            ->to($to)
-            ->subject('Votre compte sur la plateforme de réservation de matériel')
-            ->htmlTemplate('email/transactional/materiel-account-creation.html.twig')
-            ->context([
+        $this->mailer->send(
+            $to,
+            'transactional/materiel-account-creation',
+            [
                 'firstName' => $firstName,
                 'lastName' => $lastName,
                 'userEmail' => $credentials['email'],
                 'password' => $credentials['password'],
                 'pseudo' => $credentials['pseudo'],
                 'platformUrl' => $this->materielPlatformUrl,
-            ]);
-
-        $this->mailer->send($email);
+            ]
+        );
     }
 }
