8db6403e2cfa455c509cea09de51d19a1094f1ee
Kévin Liagre <kevin.liagre@gmail.com>
2025-05-17 08:59:16 +0200

    Add reusable generic upload component (#971)

---
+++ src/Controller/MediaUploadController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MediaUpload;
+use App\Repository\MediaUploadRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Liip\ImagineBundle\Exception\Binary\Loader\NotLoadableException;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Vich\UploaderBundle\Handler\UploadHandler;
+
+#[AsController]
+class MediaUploadController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private MediaUploadRepository $mediaUploadRepository,
+        private Security $security,
+        private UploadHandler $uploadHandler,
+        private CacheManager $imagineCacheManager
+    ) {
+    }
+
+    #[Route('/upload-image', name: 'media_upload_image', methods: ['POST'])]
+    public function __invoke(Request $request, ValidatorInterface $validator): Response
+    {
+        return $this->upload($request, $validator, new File([
+            'maxSize' => '5M',
+            'extensions' => ['jpg', 'jpeg', 'png', 'gif', 'webp'],
+        ]));
+    }
+
+    #[Route('/upload-file', name: 'media_upload_file', methods: ['POST'])]
+    public function uploadFile(Request $request, ValidatorInterface $validator): Response
+    {
+        return $this->upload($request, $validator, new File([
+            'maxSize' => '5M',
+            'extensions' => ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'],
+        ]));
+    }
+
+    public function upload(Request $request, ValidatorInterface $validator, File $fileConstraints): Response
+    {
+        $user = $this->security->getUser();
+        if (!$user) {
+            throw $this->createAccessDeniedException('User must be logged in');
+        }
+
+        $file = $request->files->get('file');
+        if (!$file) {
+            throw new BadRequestHttpException('File is required');
+        }
+
+        $errors = $validator->validate($file, $fileConstraints);
+
+        if ($errors->count() > 0) {
+            throw new BadRequestHttpException((string) $errors);
+        }
+
+        $mediaUpload = new MediaUpload();
+        $mediaUpload->setUploadedBy($user);
+        $mediaUpload->setFile($file);
+
+        $this->entityManager->persist($mediaUpload);
+        $this->entityManager->flush();
+
+        $this->uploadHandler->upload($mediaUpload, 'file');
+
+        $this->entityManager->flush();
+
+        $imagePath = $request->getSchemeAndHttpHost() . '/uploads/files/' . $mediaUpload->getFilename();
+
+        $thumbnails = [];
+        if (\in_array(strtolower($file->getClientOriginalExtension()), ['jpg', 'jpeg', 'png', 'gif', 'webp'], true)) {
+            try {
+                // Chemin relatif pour LiipImagine
+                $relativeImagePath = 'uploads/files/' . $mediaUpload->getFilename();
+
+                // Générer les URLs des thumbnails
+                $thumbnails = [
+                    'wide' => $this->imagineCacheManager->getBrowserPath($relativeImagePath, 'wide_thumbnail'),
+                    'min' => $this->imagineCacheManager->getBrowserPath($relativeImagePath, 'min_thumbnail'),
+                ];
+            } catch (NotLoadableException $e) {
+                throw new BadRequestHttpException('Image non valide');
+            }
+        }
+
+        return $this->json([
+            'id' => $mediaUpload->getId(),
+            'filename' => $mediaUpload->getFilename(),
+            'url' => $imagePath,
+            'thumbnails' => $thumbnails,
+            'createdAt' => $mediaUpload->getCreatedAt()->format('c'),
+        ], Response::HTTP_CREATED);
+    }
+}
