6f2ea2dd4d5554f7dd053fb163aaf7b10b2fa30d
Kévin Liagre <kevin.liagre@gmail.com>
2025-05-28 00:06:55 +0200

    PoC for single modal in Twig

---
+++ assets/js/modal/modal.js
@@ -0,0 +1,24 @@
+import MicroModal from 'micromodal';
+
+class Modal {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        MicroModal.init({
+            disableScroll: true,
+            debugMode: true
+        });
+    }
+
+    show(content, title = '') {
+        document.getElementById('app-modal-title').textContent = title;
+        document.getElementById('app-modal-content').innerHTML = content;
+        MicroModal.show('app-modal');
+    }
+}
+
+const modal = new Modal();
+
+window.modal = modal;
