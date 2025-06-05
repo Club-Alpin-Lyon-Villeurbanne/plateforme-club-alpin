a1e2828afefde5425dc02c8e6b0115a588f78ecb
Kévin Liagre <kevin.liagre@gmail.com>
2025-06-05 12:15:22 +0200

    PoC for single modal in Twig (#1008)

---
+++ src/Legacy/LegacyEntrypointRenderer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Legacy;
+
+use Pentatrion\ViteBundle\Service\EntrypointRenderer;
+
+/**
+ * @phpstan-type ViteEntryScriptTagsOptions array{
+ *  absolute_url?: bool,
+ *  attr?: array<string, bool|string|null>,
+ *  dependency?: "react"|null
+ * }
+ * @phpstan-type ViteEntryLinkTagsOptions array{
+ *  absolute_url?: bool,
+ *  attr?: array<string, bool|string|null>,
+ *  preloadDynamicImports?: bool
+ * }
+ */
+class LegacyEntrypointRenderer
+{
+    public function __construct(private EntrypointRenderer $entrypointRenderer)
+    {
+    }
+
+    /**
+     * @param ViteEntryScriptTagsOptions $options
+     */
+    public function renderViteScriptTags(string $entryName, array $options = [], ?string $configName = null): string
+    {
+        return $this->entrypointRenderer->renderScripts($entryName, $options, $configName);
+    }
+
+    /**
+     * @param ViteEntryLinkTagsOptions $options
+     */
+    public function renderViteLinkTags(string $entryName, array $options = [], ?string $configName = null): string
+    {
+        return $this->entrypointRenderer->renderLinks($entryName, $options, $configName);
+    }
+}
