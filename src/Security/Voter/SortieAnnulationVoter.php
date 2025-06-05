2030aa0d84b04d10d1d4086d9d043ddca93bdc59
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-06-05 11:53:54 +0200

    fix: droits d'annulation de sortie

--- src/Security/Voter/SortieAnnulationVoter.php
+++ src/Security/Voter/SortieAnnulationVoter.php
@@ -2,10 +2,8 @@
 
 namespace App\Security\Voter;
 
-use App\Entity\EventParticipation;
 use App\Entity\Evt;
 use App\Entity\User;
-use App\Entity\UserAttr;
 use App\UserRights;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
@@ -55,24 +53,6 @@ class SortieAnnulationVoter extends Voter
             return false;
         }
 
-        $amIEncadrant = false;
-        foreach ($subject->getEncadrants([EventParticipation::ROLE_ENCADRANT]) as $eventParticipation) {
-            if ($eventParticipation->getUser() === $user) {
-                $amIEncadrant = true;
-            }
-        }
-
-        $amIResponsable = false;
-        foreach ($user->getAttributes() as $attribute) {
-            if (UserAttr::RESPONSABLE_COMMISSION === $attribute->getUserType()->getCode()) {
-                $amIResponsable = true;
-            }
-        }
-
-        if (($amIEncadrant || $amIResponsable) && $this->userRights->allowedOnCommission('evt_cancel', $subject->getCommission())) {
-            return true;
-        }
-
-        return false;
+        return ($this->userRights->allowedOnCommission('evt_cancel', $subject->getCommission()) || $this->userRights->allowed('evt_cancel_any'));
     }
 }
