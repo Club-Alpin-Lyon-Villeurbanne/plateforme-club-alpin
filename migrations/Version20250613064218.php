081e3bc65618bf36d72b9fc7f1cb6cd3c659fac0
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-06-13 08:44:08 +0200

    feat: formulaire sortie en Sf 6.4

--- migrations/Version20250613064218.php
+++ migrations/Version20250613064218.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20250512153454 extends AbstractMigration
+final class Version20250613064218 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,16 @@ final class Version20250512153454 extends AbstractMigration
         $this->addSql('UPDATE caf_evt SET event_start_date = FROM_UNIXTIME(tsp_evt) WHERE tsp_evt IS NOT NULL');
         $this->addSql('UPDATE caf_evt SET event_end_date = FROM_UNIXTIME(tsp_end_evt) WHERE tsp_end_evt IS NOT NULL');
         $this->addSql('UPDATE caf_evt SET join_start_date = FROM_UNIXTIME(join_start_evt) WHERE join_start_evt IS NOT NULL');
+        $this->addSql('ALTER TABLE caf_evt ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE caf_evt SET created_at = FROM_UNIXTIME(tsp_crea_evt) WHERE tsp_crea_evt IS NOT NULL');
+        $this->addSql('UPDATE caf_evt SET updated_at = created_at WHERE tsp_edit_evt IS NULL');
+        $this->addSql('UPDATE caf_evt SET updated_at = FROM_UNIXTIME(tsp_edit_evt) WHERE tsp_edit_evt IS NOT NULL');
+        $this->addSql('ALTER TABLE caf_evt CHANGE created_at created_at DATETIME NOT NULL, CHANGE updated_at updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE caf_evt DROP event_start_date, DROP event_end_date, DROP join_start_date');
+        $this->addSql('ALTER TABLE caf_evt DROP created_at, DROP updated_at');
     }
 }
