9e16726f1bbd17fd3585a9fa104ed27f1b54553e
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-06-20 11:14:56 +0000

    fix: recherche (#1091)

--- legacy/pages/recherche.php
+++ legacy/pages/recherche.php
@@ -18,11 +18,11 @@ if ('recherche' == $p1 && isset($_GET['str']) && strlen($_GET['str'])) {
         $req = 'SELECT
                 SQL_CALC_FOUND_ROWS
                 `id_article` ,  `tsp_article` ,  `user_article` ,  `status_article` ,  `titre_article` ,  `code_article` ,  `commission_article` ,  `une_article` ,  `cont_article`
-                , nickname_user, id_user, media_upload_id
-            FROM caf_article, caf_user
-            LEFT JOIN media_upload m ON caf_article.media_upload_id = m.id
+                , nickname_user, id_user, media_upload_id, m.filename
+            FROM caf_article AS a
+            LEFT JOIN caf_user as u  ON a.user_article = u.id_user
+            LEFT JOIN media_upload m ON a.media_upload_id = m.id
             WHERE  `status_article` =1
-            AND user_article = id_user
             AND status_article = 1
             '
             // commission donnée : filtre (mais on inclut les actus club, commission=0)
