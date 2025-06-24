4d537add23bc7594757444b59e7bd31e7a492625
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-06-24 15:24:16 +0200

    feat: nouvel éditeur wysiwyg

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -14,7 +14,6 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
