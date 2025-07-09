7d852beed88cb6e957b063dec14e579b40f8885a
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-07-09 13:06:20 +0000

    Revert "fix: runtime with default values for var" (#1146)

--- public/index.php
+++ public/index.php
@@ -7,29 +7,7 @@ require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 return function (array $context) {
     global $kernel;
 
-    // PATCH TEMPORAIRE - Debug Clever Cloud bug 2025-07-09
-    
-    // Essayer de récupérer APP_ENV de toutes les sources possibles
-    $appEnv = $context['APP_ENV'] 
-        ?? getenv('APP_ENV') 
-        ?? $_ENV['APP_ENV'] 
-        ?? $_SERVER['APP_ENV'] 
-        ?? null;
-    
-    // Si toujours vide, forcer prod
-    if (empty($appEnv)) {
-        error_log('WARNING: APP_ENV is empty or not found, forcing to prod');
-        $appEnv = 'prod';
-    }
-    
-    // Même chose pour APP_DEBUG
-    $appDebug = $context['APP_DEBUG'] 
-        ?? getenv('APP_DEBUG') 
-        ?? $_ENV['APP_DEBUG'] 
-        ?? $_SERVER['APP_DEBUG'] 
-        ?? '0';
-    
-    $kernel = new Kernel($appEnv, (bool) $appDebug);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 
     return $kernel;
 };
