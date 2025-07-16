e0208c3b95228d7929afc930f4d8db0efc5dd974
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-07-16 08:30:15 +0200

    feat: retirer la notion de groupe dans les sorties (#1160)

--- src/Controller/CommissionController.php
+++ src/Controller/CommissionController.php
@@ -3,11 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Commission;
-use App\Entity\Groupe;
 use App\Service\ParticipantService;
-use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormFactoryInterface;
@@ -17,40 +14,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CommissionController extends AbstractController
 {
-    #[Route('/groupes-par-commission', name: 'groups_by_commission')]
-    public function groupsByCommission(
-        Request $request,
-        ManagerRegistry $doctrine,
-        FormFactoryInterface $formFactory
-    ): Response {
-        $commissionId = $request->query->get('commission');
-        $commission = $doctrine->getRepository(Commission::class)->find($commissionId);
-
-        $form = $formFactory->createBuilder()
-            ->add('groupe', EntityType::class, [
-                'class' => Groupe::class,
-                'query_builder' => function (EntityRepository $er) use ($commission) {
-                    return $er->createQueryBuilder('g')
-                              ->where('g.actif = 1')
-                              ->andWhere('g.idCommission = :commission')
-                              ->setParameters(['commission' => $commission])
-                              ->orderBy('g.nom', 'ASC')
-                    ;
-                },
-                'label' => 'Groupe concerné par cette sortie',
-                'required' => false,
-                'attr' => [
-                    'class' => 'type1 wide',
-                ],
-            ])
-            ->getForm()
-        ;
-
-        return $this->render('form/field_group.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
     #[Route('/encadrement-par-commission', name: 'participants_by_commission')]
     public function participantsByCommission(
         Request $request,
