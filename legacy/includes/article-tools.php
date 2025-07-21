e40e5482bd02d8d31624e49f116f401f1149ce17
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-07-21 10:43:20 +0200

    fix: afficher correctement les brouillons

--- legacy/includes/article-tools.php
+++ legacy/includes/article-tools.php
@@ -4,16 +4,16 @@ if (user()) {
 	<div class="article-tools">
 
 		<?php
-        // statut de l'article
-        if (0 == $article['topubly_article']) {
-            echo "<p class='draft'>Brouillon (non publié)</p>";
-        } elseif (0 == $article['status_article']) {
-            echo "<p class='alerte'>En attente : cet article n'a pas encore été publié par un responsable.</p>";
-        } elseif (1 == $article['status_article']) {
-            echo '<p class="info">Publié : cet article est en ligne</p>';
-        } elseif (2 == $article['status_article']) {
-            echo "<p class='erreur'>Désactivé : cet article a été refusé par un responsable</p>";
-        }
+    // statut de l'article
+    if (1 == $article['status_article']) {
+        echo '<p class="info">Publié : cet article est en ligne</p>';
+    } elseif (2 == $article['status_article']) {
+        echo "<p class='erreur'>Désactivé : cet article a été refusé par un responsable</p>";
+    } elseif (0 == $article['topubly_article']) {
+        echo "<p class='draft'>Brouillon (non publié)</p>";
+    } elseif (0 == $article['status_article'] && 1 == $article['topubly_article']) {
+        echo "<p class='alerte'>En attente : cet article n'a pas encore été publié par un responsable.</p>";
+    }
 
     // BOUTONS
     // publié ? voir
@@ -34,24 +34,16 @@ if (user()) {
 			<?php
     }
 
-    // lier des co-rédacs
-    /*
+    // on peut toujours modifier
     ?>
-    <a href="javascript:void(0)" title="" class="nice2">
-        Co-rédacteurs
-    </a>
-    <?php
-    */
-
-    // on peut toujours modifier?>
 		<a href="/article/<?php echo (int) $article['id_article']; ?>/edit" title="" class="nice2 orange">
 			Modifier
 		</a>
 		<?php
 
-        // si publié : dépublier
-        if (1 == $article['status_article']) {
-            ?>
+    // si publié : dépublier
+    if (1 == $article['status_article']) {
+        ?>
 			<a href="javascript:$.fancybox($('#depublier-form-<?php echo $article['id_article']; ?>').html());" title="" class="nice2 red" id="button-depublier">
 				Dépublier
 			</a>
@@ -67,7 +59,7 @@ if (user()) {
 				</form>
 			</div>
 			<?php
-        }
+    }
 
     // si dépublié : supprimer
     if (1 != $article['status_article']) {
