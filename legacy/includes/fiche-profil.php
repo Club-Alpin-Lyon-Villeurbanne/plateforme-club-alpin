5b6f9c59fe63d32a89055eaf9ee8cd241b7880fc
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-07-28 12:32:47 +0200

    fix: ne rien afficher si pas de date d'adhésion (#1202)

--- legacy/includes/fiche-profil.php
+++ legacy/includes/fiche-profil.php
@@ -124,7 +124,7 @@ else {
 		             . '<li>NUMÉRO DE LICENCE : ' . html_utf8($tmpUser['cafnum_user']) . '</a> </li>';
 		        if (allowed('user_read_private') && $tmpUser['doit_renouveler_user']) {
 		            echo '<li class="red">LICENCE EXPIRÉE</li>';
-		        } elseif (allowed('user_read_private')) {
+		        } elseif (allowed('user_read_private') && !empty($tmpUser['date_adhesion_user'])) {
 		            echo '<li>DATE D\'ADHÉSION : <span class="green">' . date('d/m/Y', $tmpUser['date_adhesion_user']) . '</span></li>';
 		        }
 		        echo '<li><a href="mailto:' . html_utf8($tmpUser['email_user']) . '" title="Contact direct">' . html_utf8($tmpUser['email_user']) . '</a> </li>'
