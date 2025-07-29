0952192a2714ebadf88fcade851408013a2dc35b
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-07-29 21:12:47 +0200

    [mobile] Fix PushNotificationService & remove now useless PushNotificationController

--- src/Service/PushNotificationService.php
+++ src/Service/PushNotificationService.php
@@ -4,19 +4,17 @@ namespace App\Service;
 
 use App\Entity\Article;
 use App\Entity\Evt;
-use App\Entity\User;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
 use Liip\ImagineBundle\Imagine\Cache\CacheManager;
-use Symfony\Bundle\SecurityBundle\Security;
 
 class PushNotificationService
 {
     private Messaging $messaging;
 
-    public function __construct(private Security $security, private CacheManager $cache)
+    public function __construct(private CacheManager $cache, private string $appInstanceId, private string $appDeeplinkScheme, private string $sitename)
     {
         $factory = new Factory();
         $this->messaging = $factory->createMessaging();
@@ -30,42 +28,28 @@ class PushNotificationService
         ?array $data = []
     ) {
         $message = CloudMessage::new()
-            ->withNotification(Notification::create($_ENV['SITENAME'] . ' - ' . $title, $body, $imageUrl))
+            ->withNotification(Notification::create($this->sitename . ' - ' . $title, $body, $imageUrl))
             ->withData($data)
-            ->toTopic($_ENV['APP_INSTANCE'] . '_' . $topic);
+            ->toTopic($this->appInstanceId . '_' . $topic);
 
         return $this->messaging->send($message);
     }
 
-    private function getUserId(): ?string
-    {
-        /** @var User? $user */
-        $user = $this->security->getUser() instanceof User ? $this->security->getUser() : null;
-        if ($user) {
-            return (string) $user->getId();
-        }
-
-        return null;
-    }
-
     public function notifyArticle(Article $article)
     {
-        $userId = $this->getUserId();
-        if ($userId) {
-            $thumbnail = $article->getMediaUpload();
-            $thumbnailUrl = $thumbnail ? $this->cache->generateUrl('uploads/files/' . $thumbnail->getFilename(), 'wide_thumbnail') : null;
-            $this->sendNotificationToTopic('article_all', 'Nouvel article', $article->getTitre(), $thumbnailUrl, [
-                'url' => $_ENV['APP_DEEPLINK_SCHEME'] . '://article/' . $article->getId(),
-                'instanceId' => $_ENV['APP_INSTANCE'],
-                'userId' => $userId,
-            ]);
-        }
+        $thumbnail = $article->getMediaUpload();
+        $thumbnailUrl = $thumbnail ? $this->cache->generateUrl('uploads/files/' . $thumbnail->getFilename(), 'wide_thumbnail') : null;
+        $this->sendNotificationToTopic('article_all', 'Nouvel article', $article->getTitre(), $thumbnailUrl, [
+            'url' => $this->appDeeplinkScheme . '://article/' . $article->getId(),
+            'instanceId' => $this->appInstanceId,
+        ]);
     }
 
     public function notifyEvent(Evt $event)
     {
         $this->sendNotificationToTopic('event_' . $event->getCommission()->getCode(), 'Nouvelle sortie', $event->getCommission()->getTitle() . ': ' . $event->getTitre(), null, [
-            'url' => $_ENV['APP_DEEPLINK_SCHEME'] . '://event/' . $event->getId(),
+            'url' => $this->appDeeplinkScheme . '://event/' . $event->getId(),
+            'instanceId' => $this->appInstanceId,
         ]);
     }
 }
