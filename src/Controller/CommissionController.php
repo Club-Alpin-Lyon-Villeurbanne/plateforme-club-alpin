1d26a29a3029750c367c2c5a144226d4dfcaa454
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-07-31 13:56:32 +0200

    feat: inciter à rédiger des CRs (#1197)

--- src/Controller/CommissionController.php
+++ src/Controller/CommissionController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Commission;
+use App\Entity\Evt;
+use App\Helper\MonthHelper;
+use App\Repository\EvtRepository;
 use App\Service\ParticipantService;
+use App\UserRights;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormFactoryInterface;
@@ -58,6 +63,53 @@ class CommissionController extends AbstractController
         ;
 
         return $this->render('form/field_participants.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/sorties-par-commission', name: 'sorties_by_commission')]
+    public function eventsByCommission(
+        Request $request,
+        ManagerRegistry $doctrine,
+        EvtRepository $eventRepository,
+        UserRights $userRights,
+        MonthHelper $monthHelper,
+        FormFactoryInterface $formFactory
+    ): Response {
+        $commissionId = $request->query->get('commission');
+        $commission = $doctrine->getRepository(Commission::class)->find($commissionId);
+
+        $form = $formFactory->createBuilder()
+            ->add('event', EntityType::class, [
+                'class' => Evt::class,
+                'choices' => array_filter(
+                    $eventRepository->getRecentPastEvents($commission),
+                    fn (Evt $event) => $userRights->allowedOnCommission('evt_create', $event->getCommission())
+                ),
+                'choice_label' => function (Evt $evt) use ($monthHelper) {
+                    return date('d', $evt->getTsp()) . ' ' .
+                       $monthHelper->getMonthName(date('m', $evt->getTsp())) . ' ' .
+                       date('Y', $evt->getTsp()) . ' | ' .
+                       $evt->getCommission()->getTitle() . ' | ' .
+                       $evt->getTitre()
+                    ;
+                },
+                'placeholder' => 'Sélectionner',
+                'required' => true,
+                'label' => 'Lier cet article à une sortie',
+                'attr' => [
+                    'class' => 'type1 wide',
+                    'style' => 'width: 95%',
+                ],
+                'help' => 'Champ obligatoire pour un compte rendu de sortie.',
+                'help_attr' => [
+                    'class' => 'mini',
+                ],
+            ])
+            ->getForm()
+        ;
+
+        return $this->render('form/field_events.html.twig', [
             'form' => $form->createView(),
         ]);
     }
