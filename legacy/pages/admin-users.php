16d1bae969cb197c762acf477bdc769cade4190a
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-09-02 10:35:17 +0200

    fix: caractères accentués dans les noms de famille en majuscules (#1237)

--- legacy/pages/admin-users.php
+++ legacy/pages/admin-users.php
@@ -184,8 +184,8 @@ if (!isGranted(SecurityConstants::ROLE_ADMIN)) {
             . '</td>'
             . '<td>' . (int) $elt['valid_user'] . '</td>'
             . '<td>' . html_utf8($elt['civ_user']) . '</td>'
-            . '<td>' . strtoupper(html_utf8($elt['lastname_user'])) . '</td>'
-            . '<td>' . ucfirst(html_utf8($elt['firstname_user'])) . '</td>';
+            . '<td>' . html_utf8(strtoupper($elt['lastname_user'])) . '</td>'
+            . '<td>' . html_utf8(ucfirst($elt['firstname_user'])) . '</td>';
 
         if ($elt['doit_renouveler_user']) {
             echo '<td style="color:red">Licence expirée</td>';
