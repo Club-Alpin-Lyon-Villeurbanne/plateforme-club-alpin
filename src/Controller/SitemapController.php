7dfbcbdfc530161e8b640220e8012c2de94c29d4
Nicolas Ritouet <nicolas@ritouet.com>
2025-09-03 23:09:58 +0200

    chore(seo): use #[AsController] for SitemapController and remove services.yaml controller block

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -9,9 +9,11 @@ use App\Repository\CommissionRepository;
 use App\Repository\PageRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+#[AsController]
 class SitemapController
 {
     public function __construct(
