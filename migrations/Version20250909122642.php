ee30d257f15e9df856c2672638e1f56d9a211abd
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-09-09 14:47:17 +0200

    feat: disclaimer images sur dépôt sortie

---
+++ migrations/Version20250909122642.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250909122642 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt ADD images_authorized TINYINT(1) DEFAULT 0 NOT NULL, ADD agree_edito TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt DROP agree_edito, DROP images_authorized');
+    }
+}
