aaf59d045f845305a24fb917a5a8ce66012b4eb7
Nicolas Ritouet <nicolas@ritouet.com>
2025-09-13 21:19:33 +0200

    revert: remove healthcheck changes from other cron scripts
    Keep only the backup-database.sh changes for this PR

--- clevercloud/crons/save-images.sh
+++ clevercloud/crons/save-images.sh
@@ -1,15 +1,8 @@
-#!/bin/bash -l
+#! /bin/bash -l
 
 # Important to keep this file
 # Clevercloud requires this to load environment
 # https://developers.clever-cloud.com/doc/administrate/cron/#access-environment-variables
-
-# Source common utilities for healthcheck monitoring
-source ${ROOT}/clevercloud/crons/common.sh
-
-# Initialize healthcheck monitoring
-# Requires HEALTHCHECK_SAVE_IMAGES env var to be set with healthchecks.io UUID
-init_healthcheck "HEALTHCHECK_SAVE_IMAGES" "save-images"
 
 cd ${APP_HOME}
 bin/console save-images
--- clevercloud/crons/sync-members.sh
+++ clevercloud/crons/sync-members.sh
@@ -1,15 +1,8 @@
-#!/bin/bash -l
+#! /bin/bash -l
 
 # Important to keep this file
 # Clevercloud requires this to load environment
 # https://developers.clever-cloud.com/doc/administrate/cron/#access-environment-variables
-
-# Source common utilities for healthcheck monitoring
-source ${ROOT}/clevercloud/crons/common.sh
-
-# Initialize healthcheck monitoring
-# Requires HEALTHCHECK_SYNC_MEMBERS env var to be set with healthchecks.io UUID
-init_healthcheck "HEALTHCHECK_SYNC_MEMBERS" "sync-members"
 
 cd ${APP_HOME}
 bin/console ffcam-file-sync
