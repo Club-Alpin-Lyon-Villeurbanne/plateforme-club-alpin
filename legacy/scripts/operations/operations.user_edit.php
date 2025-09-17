1cb4b5428e6df0ab2d9a306f8c084f94423c8632
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-09-17 15:37:22 +0200

    fix: modif adhérent

--- legacy/scripts/operations/operations.user_edit.php
+++ legacy/scripts/operations/operations.user_edit.php
@@ -4,7 +4,6 @@ use App\Legacy\LegacyContainer;
 
 $userTab = [];
 $userTab['cafnum_user'] = trim(stripslashes($_POST['cafnum_user']));
-$userTab['cafnum_user_new'] = trim(stripslashes($_POST['cafnum_user_new']));
 $userTab['email_user'] = trim(stripslashes($_POST['email_user']));
 $userTab['mdp_user'] = trim(stripslashes($_POST['mdp_user']));
 $userTab['id_user'] = trim(stripslashes($_POST['id_user']));
@@ -23,15 +22,7 @@ if (!preg_match('/^(N_D)?\d{12}$/', $userTab['cafnum_user'])) {
     $errTab[] = "Numéro d'adhérent invalide : " . $userTab['cafnum_user'];
 }
 
-if (0 != strlen($userTab['cafnum_user_new']) && ((!is_numeric($userTab['cafnum_user_new'])) || 12 != strlen($userTab['cafnum_user_new']))) {
-    $errTab[] = "Nouveau numéro d'adhérent invalide : " . $userTab['cafnum_user_new'];
-}
-
 if (!isset($errTab) || 0 === count($errTab)) {
-    if ($userTab['cafnum_user_new'] > 0) {
-        LegacyContainer::get('legacy_member_merger')->mergeExistingMembers($userTab['cafnum_user'], $userTab['cafnum_user_new']);
-        $userTab['cafnum_user'] = $userTab['cafnum_user_new'];
-    }
 
     $stmt = LegacyContainer::get('legacy_mysqli_handler')->prepare('UPDATE `caf_user` SET email_user=?, auth_contact_user=? WHERE cafnum_user=?');
     $stmt->bind_param('sss', $userTab['email_user'], $userTab['auth_contact_user'], $userTab['cafnum_user']);
