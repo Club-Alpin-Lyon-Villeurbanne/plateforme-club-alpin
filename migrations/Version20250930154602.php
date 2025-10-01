f24f8a1964f9e4173aaddbb01f782cd6306c12d6
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-10-01 08:36:35 +0200

    feat: datetime au lieu de timestamp

---
+++ migrations/Version20250930154602.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250930154602 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Removes no longer used fields';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt_join DROP tsp_evt_join, DROP lastchange_when_evt_join');
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
