25f36adbf2a73d2ac1e4a1f67e1a06ebd8f75b96
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-10-01 16:49:14 +0200

    feat: datetime au lieu de timestamp

--- migrations/Version20250930154602.php
+++ migrations/Version20250930154602.php
@@ -17,10 +17,15 @@ final class Version20250930154602 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE caf_evt_join DROP tsp_evt_join, DROP lastchange_when_evt_join');
-        $this->addSql('ALTER TABLE caf_article DROP tsp_crea_article, DROP tsp_article');
+        $this->addSql('ALTER TABLE caf_article DROP tsp_crea_article, DROP tsp_validate_article, DROP tsp_article, DROP tsp_lastedit');
+        $this->addSql('ALTER TABLE caf_comment DROP tsp_comment');
     }
 
     public function down(Schema $schema): void
     {
+
+        $this->addSql('ALTER TABLE caf_evt_join ADD tsp_evt_join INT NOT NULL, ADD lastchange_when_evt_join INT DEFAULT NULL COMMENT \'Quand a été modifié cet élément\'');
+        $this->addSql('ALTER TABLE caf_article ADD tsp_crea_article INT NOT NULL COMMENT \'Timestamp de création de l\'\'article\', ADD tsp_validate_article INT DEFAULT NULL, ADD tsp_article INT NOT NULL COMMENT \'Timestamp affiché de l\'\'article\', ADD tsp_lastedit DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT \'Date de dernière modif\'');
+        $this->addSql('ALTER TABLE caf_comment ADD tsp_comment BIGINT NOT NULL');
     }
 }
