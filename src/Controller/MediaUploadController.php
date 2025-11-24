9d5c1a5078b061deb261ed68d33649a31037435e
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-11-24 14:57:02 +0100

    fix: php attribute au lieu d'annotation (#1498)

--- src/Controller/MediaUploadController.php
+++ src/Controller/MediaUploadController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Vich\UploaderBundle\Handler\UploadHandler;
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\NoResultException;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(path: '/profil', methods: ['GET'])]
