d5fd7f670349e4b978738bf354ed2e8359d0772e
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-12-01 15:56:34 +0100

    fix: ne pas afficher si sortie brouillon

--- src/Security/Voter/SortieViewVoter.php
+++ src/Security/Voter/SortieViewVoter.php
@@ -31,7 +31,7 @@ class SortieViewVoter extends Voter
             return true;
         }
 
-        if ($this->userRights->allowed('evt_validate')) {
+        if ($this->userRights->allowed('evt_validate', 'commission:' . $subject->getCommission()->getCode())) {
             return true;
         }
 
