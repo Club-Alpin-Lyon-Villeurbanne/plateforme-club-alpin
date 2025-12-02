226d1347bd6b3dd2e0425a6220e6379e1910c57e
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-12-02 09:47:02 +0100

    fix: droits création sortie

---
+++ src/Security/Voter/SortieCreateVoter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Commission;
+use App\Entity\User;
+use App\UserRights;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class SortieCreateVoter extends Voter
+{
+    private UserRights $userRights;
+
+    public function __construct(UserRights $userRights)
+    {
+        $this->userRights = $userRights;
+    }
+
+    protected function supports($attribute, $subject): bool
+    {
+        return \in_array($attribute, ['SORTIE_CREATE'], true);
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if (!$subject instanceof Commission) {
+            return $this->userRights->allowed('evt_create');
+        }
+
+        return $this->userRights->allowedOnCommission('evt_create', $subject);
+    }
+}
