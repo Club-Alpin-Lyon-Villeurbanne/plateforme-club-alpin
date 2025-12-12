4acf651f195374615a5b55f2d28eb6dc290bbe27
Del Plop <71543137+DelPlop@users.noreply.github.com>
2025-12-12 16:40:43 +0100

    fix: cron pour anonymisation automatique des utilisateurs

---
+++ clevercloud/crons/anonymize-users.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -l
+
+# Important to keep this file
+# Clevercloud requires this to load environment
+# https://developers.clever-cloud.com/doc/administrate/cron/#access-environment-variables
+
+cd ${APP_HOME}
+bin/console user-anonymization-cron
