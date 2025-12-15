7230fc44ba72f28d74e993805fa356d9236f15ae
DelPlop <71543137+DelPlop@users.noreply.github.com>
2025-12-15 13:48:04 +0100

    fix: les crons ne doivent s'exécuter qu'en prod (#1566)

--- clevercloud/crons/save-images.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -l
-
-# Important to keep this file
-# Clevercloud requires this to load environment
-# https://developers.clever-cloud.com/doc/administrate/cron/#access-environment-variables
-
-cd ${APP_HOME}
-bin/console save-images
