e94e3c0cc6a70c6dde3025b44b8d8f483825cec1
Del Plop <71543137+DelPlop@users.noreply.github.com>
2026-01-09 08:31:33 +0100

    feat: refonte mes articles en Symfony

--- legacy/scripts/fonctions.php
+++ legacy/scripts/fonctions.php
@@ -3,61 +3,6 @@
 use App\Legacy\LegacyContainer;
 
 /*
-    Récupération des sorties d'un utilisateur
-    ET affichage
-*/
-function display_sorties($id_user, $limit = 10, $title = '')
-{
-    $req = '
-        SELECT SQL_CALC_FOUND_ROWS
-            id_evt, code_evt, status_evt, status_evt_join, status_legal_evt, cancelled_evt, user_evt, commission_evt, start_date, end_date, caf_evt.created_at, caf_evt.updated_at, place_evt, rdv_evt, titre_evt, tarif_evt, join_max_evt, join_start_date, ngens_max_evt, is_draft
-            , nickname_user
-            , title_commission, code_commission
-            , role_evt_join
-        FROM caf_evt
-            , caf_user
-            , caf_commission
-            , caf_evt_join
-        WHERE status_evt=1
-        AND id_user = user_evt
-        AND id_commission = commission_evt '
-        // jointure avec la table participation
-        . 'AND evt_evt_join = id_evt
-        AND user_evt_join = ' . $id_user
-        // de la plus récente a la plus ancienne
-        . ' ORDER BY  `start_date` DESC
-        LIMIT ' . $limit;
-    // echo $req;
-    $handleSql = LegacyContainer::get('legacy_mysqli_handler')->query($req);
-    // calcul du total grace à SQL_CALC_FOUND_ROWS
-    $totalSql = LegacyContainer::get('legacy_mysqli_handler')->query('SELECT FOUND_ROWS()');
-    $total = getArrayFirstValue($totalSql->fetch_array(\MYSQLI_NUM));
-
-    // compte :
-    if ($total > 0) {
-        echo '<h2 id="user-sorties">' . $title . ' :</h2>';
-        echo '<p class="mini">' . $total . ' sortie(s) en tout</p>';
-        echo '<div style="width:620px">';
-        // liste
-        echo '<table id="agenda">';
-        while ($handle = $handleSql->fetch_array(\MYSQLI_ASSOC)) {
-            // compte places totales, données stockées dans $handle['temoin'] && $handle['temoin-title']
-            require __DIR__ . '/../includes/evt-temoin-reqs.php';
-
-            $evt = $handle;
-
-            echo '<tr>'
-                    . '<td class="agenda-gauche">' . (new DateTimeImmutable($evt['start_date']))?->format('d/m/Y') . '</td>'
-                    . '<td>';
-            require __DIR__ . '/../includes/agenda-evt-debut.php';
-            echo '</td>'
-                . '</tr>';
-        }
-        echo '</table>';
-        echo '</div><br style="clear:both" />';
-    }
-}
-/*
     Récupération des articles publiés d'un utilisateur
     ET affichage
 */
