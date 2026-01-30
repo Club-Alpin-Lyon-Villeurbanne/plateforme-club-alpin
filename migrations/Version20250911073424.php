2821e2b41a02d5c6e087059a0ac4841d757ed5a0
Del Plop <71543137+DelPlop@users.noreply.github.com>
2026-01-30 11:39:54 +0100

    feat: bilan carbone

---
+++ migrations/Version20250911073424.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250911073424 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adds new field for carbon cost calculation to caf_evt table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt ADD main_transport_mode VARCHAR(50) DEFAULT NULL, ADD nb_vehicle INT DEFAULT NULL, ADD nb_km DOUBLE PRECISION DEFAULT NULL, ADD carbon_cost DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE caf_evt DROP main_transport_mode, nb_vehicle, nb_km, DROP carbon_cost');
+    }
+}
