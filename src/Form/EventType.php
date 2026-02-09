ddd705f9b9b8ec8d8d9adf061de0d144bb7495d1
Del Plop <71543137+DelPlop@users.noreply.github.com>
2026-02-09 10:20:09 +0100

    feat: bilan carbone

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -147,6 +147,9 @@ class EventType extends AbstractType
                     'class' => 'type2 wide',
                     'style' => 'width: 97%',
                 ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez sélectionner le mode de transport principal.']),
+                ],
                 'placeholder' => 'Choisissez un mode de transport principal',
                 'help' => 'Permet d\'aider au calcul du bilan carbone.',
                 'help_attr' => [
